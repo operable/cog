bd04c3c83e834ed3653d5b7ddf26400fa698c441
Latitia Michelle Haskins <latitia@operable.io>
2016-01-12 09:50:42 -0500

    Add tests to ensure that string to float conversions work as expected

--- test/cog/helper_test.exs
+++ test/cog/helper_test.exs
@@ -1,7 +1,7 @@
 defmodule HelperTest do
   use ExUnit.Case, async: true
 
-  import Cog.Helpers, only: [ensure_integer: 1]
+  import Cog.Helpers, only: [ensure_integer: 1, get_number: 1]
 
   test "ensure_integer should return an integer if an integer is passed" do
     assert ensure_integer(5) == 5
@@ -9,5 +9,17 @@ defmodule HelperTest do
 
   test "ensure_integer should return an integer if a binary is passed" do
     assert ensure_integer("5") == 5
+  end
+
+  test "get_number should return an integer if an integer is passed" do
+    assert get_number(5) == 5
+  end
+
+  test "get_number should return a float if a binary is passed" do
+    assert get_number("-5") == -5.0
+  end
+
+  test "get_number should return an error if a non-number is passed" do
+    assert get_number("hola") == "hola is not a number"
   end
 end
