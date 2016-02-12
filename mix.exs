4699314c5984ed162f695fa58fa4822776a31e7f
Kevin Smith <kevin@operable.io>
2016-02-12 16:30:36 -0500

    bumping Spanner dep

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule Cog.Mixfile do
      {:hedwig, "~> 0.3.0"},
      {:gproc, "~> 0.5.0", override: true},
      {:html_entities, "~> 0.2"},
-     {:spanner, git: "git@github.com:operable/spanner", ref: "0903ca63edd16941dd0c88c113df63590da26a45"},
+     {:spanner, git: "git@github.com:operable/spanner", ref: "79df643403e0ab089e95f788855671d1a98fe29b"},
      {:probe, git: "git@github.com:operable/probe", tag: "0.1-rc1"},
      {:exml, github: "paulgray/exml", tag: "2.2.1"},
      {:fumanchu, github: "operable/fumanchu", ref: "210bd6294d7fce3e9b651cc481496bf6d0cd4f1f"},
