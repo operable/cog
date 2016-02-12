9e4e193bd42cec82a3ec9485d2e5199e80523ede
vanstee <patrick@vanstee.me>
2016-02-11 21:47:30 -0500

    Convert table command to use list option

--- lib/cog/commands/table.ex
+++ lib/cog/commands/table.ex
@@ -1,5 +1,6 @@
 defmodule Cog.Commands.Table do
   use Spanner.GenCommand.Base, bundle: Cog.embedded_bundle, enforcing: false, execution: :once, calling_convention: :all
+  alias Cog.Formatters.Table
 
   @moduledoc """
   Converts lists of maps into a table of columns specified.
@@ -14,25 +15,26 @@ defmodule Cog.Commands.Table do
 
   """
 
-  option "fields", type: "string", required: true
+  option "fields", type: "list", required: true
 
-  @cell_padding 2
+  @cell_padding "  "
 
   def handle_message(req, state) do
-    headers = hd(req.options)
-    |> Map.get("fields")
-    |> String.strip(?")
-    |> String.split(~r/[\,\s]/, trim: true)
-
-    rows = Enum.map(req.cog_env, &(get_row(&1, headers)))
-    aligned_rows = Cog.Formatters.Table.format([headers|rows], @cell_padding)
-
-    {:reply, req.reply_to, "table", %{"rows" => aligned_rows}, state}
+    %{"fields" => headers} = hd(req.options)
+    table = format_table(headers, req.cog_env)
+    {:reply, req.reply_to, "raw", %{"table" => table}, state}
   end
 
-  defp get_row(item, fields) do
-    new_item = Map.take(item, fields)
-    Enum.map(fields, &(Map.get(new_item, &1)))
-  end
+  defp format_table(headers, rows) do
+    rows = Enum.map(rows, fn row ->
+      headers
+      |> Enum.into(%{}, &{&1, ""})
+      |> Map.merge(Map.take(row, headers))
+      |> Map.values
+    end)
 
+    [headers|rows]
+    |> Table.format
+    |> Enum.map_join("\n", &Enum.join(&1, @cell_padding))
+  end
 end
