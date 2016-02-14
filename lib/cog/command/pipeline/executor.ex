72ee3e87fb67b92227e4fad475b037e5b010defc
Kevin Smith <kevin@operable.io>
2016-02-14 12:59:06 -0500

    Temporary fix for template rendering bug
    See Cog issue #236 for details.
    
    This commit is a temporary fix while I continue to debug the issue.

--- lib/cog/command/pipeline/executor.ex
+++ lib/cog/command/pipeline/executor.ex
@@ -491,7 +491,11 @@ defmodule Cog.Command.Pipeline.Executor do
     # This is *NOT* a long-term solution.
     case TemplateCache.lookup(bundle_id, adapter, template) do
       fun when is_function(fun) ->
-        fun.(context)
+        if length(context) < 2 do
+          fun.(context)
+        else
+          Enum.join(Enum.map(context, fun))
+        end
       nil ->
         # Unfortunately, we don't have the bundle name or the command
         # name down here for this warning message :(
