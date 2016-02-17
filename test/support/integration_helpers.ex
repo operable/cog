b2ae4a6c30897b797ea98392d0f615d2e8b05b8f
Matthew Peck <matt@operable.io>
2016-02-17 16:15:07 -0500

    render_template test helper should render items individually
    The render_template test helper needs to render lists of input
    individually instead of as a unit. This is to match the behavior of
    templates in the main app.

--- test/support/integration_helpers.ex
+++ test/support/integration_helpers.ex
@@ -1,4 +1,6 @@
 defmodule Cog.Integration.Helpers do
+  def render_template(template_name, data) when is_list(data),
+    do: Enum.map_join(data, "\n", &(render_template(template_name, &1)))
   def render_template(template_name, data) do
     template = Cog.Repo.get_by(Cog.Models.Template, %{name: template_name, adapter: "test"})
     FuManchu.render!(template.source, data)
