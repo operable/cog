26d2d35912af1ad722c5a2d7e43b633a62d7647d
Latitia Michelle Haskins <latitia.haskins@gmail.com>
2016-02-17 18:46:38 -0500

    Update spanner ref

--- mix.exs
+++ mix.exs
@@ -67,7 +67,7 @@ defmodule Cog.Mixfile do
      {:hedwig, "~> 0.3.0"},
      {:gproc, "~> 0.5.0", override: true},
      {:html_entities, "~> 0.2"},
-     {:spanner, git: "git@github.com:operable/spanner", ref: "72d0da960b54bea722a32bfc51f6e682074ca747"},
+     {:spanner, git: "git@github.com:operable/spanner", ref: "314d40dbf326ecc29d7f9291491fb69a8fe70b34"},
      {:probe, git: "git@github.com:operable/probe", tag: "0.1-rc1"},
      {:exml, github: "paulgray/exml", tag: "2.2.1"},
      {:fumanchu, github: "operable/fumanchu", ref: "210bd6294d7fce3e9b651cc481496bf6d0cd4f1f"},
