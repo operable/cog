40892fdba26d18a954fa0af668af075672dcb579
vanstee <patrick@vanstee.me>
2016-02-18 14:47:18 -0500

    Fixup redirection to look for raw usernames and channels

--- test/support/adapters/test/api.ex
+++ test/support/adapters/test/api.ex
@@ -1,12 +1,12 @@
 defmodule Cog.Adapters.Test.API do
 
-  def lookup_room("@" <> _, as_user: _) do
+  def lookup_room("@" <> _) do
     {:ok, %{id: 1, name: "direct"}}
   end
-  def lookup_room("#" <> room, as_user: _) do
+  def lookup_room("#" <> room) do
     {:ok, %{id: 1, name: room}}
   end
-  def lookup_room(room_or_user, as_user: _) do
+  def lookup_room(room_or_user) do
     {:ok, %{id: 1, name: room_or_user}}
   end
 
