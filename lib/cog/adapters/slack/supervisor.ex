8f7b7ea81224f08c470c9528e8cc31b988e49fb4
vanstee <patrick@vanstee.me>
2016-03-02 13:20:27 -0500

    Clean up module access with alias

--- lib/cog/adapters/slack/supervisor.ex
+++ lib/cog/adapters/slack/supervisor.ex
@@ -1,21 +1,21 @@
 defmodule Cog.Adapters.Slack.Supervisor do
   use Supervisor
-
   import Cog.Helpers, only: [ensure_integer: 1]
+  alias Cog.Adapters.Slack
 
   def start_link do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init(_) do
-    config = Application.get_env(:cog, Cog.Adapters.Slack)
+    config = Application.get_env(:cog, Slack)
 
     api_token = fetch_required(config, :api_token)
     cache_ttl = ensure_integer(Keyword.get(config, :api_cache_ttl))
 
-    children = [worker(Cog.Adapters.Slack, []),
-                worker(Cog.Adapters.Slack.RTMConnector, [api_token]),
-                worker(Cog.Adapters.Slack.API, [api_token, cache_ttl])]
+    children = [worker(Slack, []),
+                worker(Slack.RTMConnector, [api_token]),
+                worker(Slack.API, [api_token, cache_ttl])]
     supervise(children, strategy: :one_for_all)
   end
 
