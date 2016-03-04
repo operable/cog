ce852d5c6681804ea3198eb4239e6d940a485c7a
vanstee <patrick@vanstee.me>
2016-03-04 10:48:12 -0500

    Make sure we include all config

--- lib/cog/adapters/irc/config.ex
+++ lib/cog/adapters/irc/config.ex
@@ -1,10 +1,12 @@
 defmodule Cog.Adapters.IRC.Config do
   @config Cog.Adapters.IRC
-  @schema [irc: [{:host,    [:required]},
-                 {:port,    [:required, :integer]},
-                 {:channel, [:required]},
-                 {:nick,    [:required]},
-                 {:use_ssl, [:required, :boolean]}]]
+  @schema [irc: [{:host,     [:required]},
+                 {:port,     [:required, :integer]},
+                 {:channel,  [:required]},
+                 {:nick,     [:required]},
+                 {:user,     []},
+                 {:password, []},
+                 {:use_ssl,  [:required, :boolean]}]]
 
   use Cog.Adapters.Config
 end
