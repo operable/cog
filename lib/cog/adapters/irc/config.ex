181f3b3043a654bd4767a69a0ae83b0f55631edd
vanstee <patrick@vanstee.me>
2016-03-08 17:06:45 -0500

    Fix IRC adapter config

--- lib/cog/adapters/irc/config.ex
+++ lib/cog/adapters/irc/config.ex
@@ -1,12 +1,13 @@
 defmodule Cog.Adapters.IRC.Config do
-  @config Cog.Adapters.IRC
-  @schema [irc: [{:host,     [:required]},
-                 {:port,     [:required, :integer]},
-                 {:channel,  [:required]},
-                 {:nick,     [:required]},
-                 {:user,     []},
-                 {:password, []},
-                 {:use_ssl,  [:required, :boolean]}]]
-
-  use Cog.Adapters.Config
+  use Cog.Adapters.Config,
+    key: Cog.Adapters.IRC,
+    schema:
+      [irc:
+        [{:host,    [:required]},
+         {:port,    [:required, :integer]},
+         {:channel, [:required]},
+         {:nick,    [:required]},
+         :user,
+         :password,
+         {:use_ssl, [:required, :boolean]}]]
 end
