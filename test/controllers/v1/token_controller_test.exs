aaebe939fd3496999aa826eba0107471dd57f2c1
Christopher Maier <chris@operable.io>
2016-03-18 17:43:43 -0400

    Tighten up some token endpoint tests

--- test/controllers/v1/token_controller_test.exs
+++ test/controllers/v1/token_controller_test.exs
@@ -22,14 +22,14 @@ defmodule Cog.V1.TokenControllerTest do
     user("tester")
     conn = post(conn, "/v1/token",
                 %{"username" => "not_tester", "password" => "tester"})
-    assert json_response(conn, 403)["errors"] != %{}
+    assert json_response(conn, 403)["error"] == "Invalid username/password"
   end
 
   test "does not create resource and renders errors when password is invalid", %{conn: conn} do
     user("tester")
     conn = post(conn, "/v1/token",
                 %{"username" => "tester", "password" => "wrong_password"})
-    assert json_response(conn, 403)["errors"] != %{}
+    assert json_response(conn, 403)["error"] == "Invalid username/password"
   end
 
 end
