c668eae5e0119b83da98ec1d1cf981f530ef80ce
vanstee <patrick@vanstee.me>
2016-03-31 11:09:33 -0500

    Make template ordering args consistent

--- lib/cog/command/pipeline/executor.ex
+++ lib/cog/command/pipeline/executor.ex
@@ -387,11 +387,11 @@ defmodule Cog.Command.Pipeline.Executor do
   # Given pipeline output, apply templating as appropriate for each
   # adapter/destination it is to be sent to, and send it to each.
   defp respond(%__MODULE__{}=state) do
-    output = state.output
-    bundle = state.current_plan.command.bundle
     destinations = state.destinations
     adapters = Map.keys(destinations)
-    rendered = render_for_adapters(output, bundle, adapters)
+    bundle = state.current_plan.command.bundle
+    output = state.output
+    rendered = render_for_adapters(adapters, bundle, output)
 
     case rendered do
       {:error, {error, template, adapter}} ->
@@ -412,12 +412,12 @@ defmodule Cog.Command.Pipeline.Executor do
 
 
   # Return a map of adapter -> rendered message
-  @spec render_for_adapters(List.t, %Cog.Models.Bundle{}, [adapter_name]) ::
+  @spec render_for_adapters([adapter_name], %Cog.Models.Bundle{}, List.t) ::
                            %{adapter_name => String.t} |
                            {:error, {term, term, term}} # {error, template, adapter}
-  defp render_for_adapters(data, bundle, adapters) do
+  defp render_for_adapters(adapters, bundle, output) do
     Enum.reduce_while(adapters, %{}, fn(adapter, acc) ->
-      case render_templates(data, bundle, adapter) do
+      case render_templates(adapter, bundle, output) do
         {:error, _}=error ->
           {:halt, error}
         message ->
@@ -428,10 +428,10 @@ defmodule Cog.Command.Pipeline.Executor do
 
   # For a specific adapter, render each output, concatenating all
   # results into a single response string
-  defp render_templates(command_output, bundle, adapter) do
-    rendered_templates = Enum.reduce_while(command_output, [], fn({data, template}, acc) ->
+  defp render_templates(adapter, bundle, output) do
+    rendered_templates = Enum.reduce_while(output, [], fn({context, template}, acc) ->
       try do
-        rendered_template = render_template(bundle, adapter, template, data)
+        rendered_template = render_template(adapter, bundle, template, context)
         {:cont, [rendered_template | acc]}
       rescue
         error ->
@@ -440,18 +440,19 @@ defmodule Cog.Command.Pipeline.Executor do
     end)
 
     case rendered_templates do
-      {:error, data} ->
-        {:error, data}
+      {:error, error} ->
+        {:error, error}
       messages ->
-        Enum.reverse(messages)
+        messages
+        |> Enum.reverse
         |> Enum.join("\n")
     end
   end
 
   # Render a single output
-  defp render_template(bundle, adapter, nil, context),
-    do: render_template(bundle, adapter, default_template(context), context)
-  defp render_template(bundle, adapter, template, context) do
+  defp render_template(adapter, bundle, nil, context),
+    do: render_template(adapter, bundle, default_template(context), context)
+  defp render_template(adapter, bundle, template, context) do
     # If `TemplateCache.lookup/3` returns nil instead of a function,
     # we know that the adapter doesn't have a template with the given
     # name. In this case, we can fall back to no template and run
@@ -463,7 +464,7 @@ defmodule Cog.Command.Pipeline.Executor do
         fun.(context)
       nil ->
         Logger.warn("The template `#{template}` was not found for adapter `#{adapter}` in bundle `#{bundle.name}`; falling back to the default")
-        render_template(bundle, adapter, nil, context)
+        render_template(adapter, bundle, nil, context)
     end
   end
 
