21622922d7593ff7e8747818c83213eb56c86ac2
Matthew Peck <matt@operable.io>
2016-04-02 11:03:37 -0400

    use with instead of plugs

--- web/controllers/v1/bundle_deploy_controller.ex
+++ web/controllers/v1/bundle_deploy_controller.ex
@@ -6,72 +6,102 @@ defmodule Cog.V1.BundleDeployController do
   plug Cog.Plug.Authentication
   plug Cog.Plug.Authorization, permission: "#{Cog.embedded_bundle}:manage_commands"
 
-  plug :validate_config_presence
-  plug :validate_file_format
-  plug :parse_config
-  plug :validate_config
-
   def deploy(conn, _) do
-    IO.inspect {"CONFIG", conn}
-    send_resp(conn, 200, "WOOT WOOT")
+    result = with {:ok, upload} <- get_upload(conn),
+                  :ok           <- validate_file_format(upload.filename),
+                  {:ok, config} <- parse_config(upload.path),
+                  :ok           <- validate_config(config),
+                  :ok           <- persist(config),
+               do: deploy(config)
+
+    case result do
+      {:ok, response} ->
+        send_success(conn, response)
+      {:error, err} ->
+        send_failure(conn, err)
+    end
   end
 
   # First let's make sure we have a file to work with
-  defp validate_config_presence(conn, _) do
+  defp get_upload(conn) do
     case conn.params do
-      %{"config_file" => %Plug.Upload{}} ->
-        conn
+      %{"config_file" => %Plug.Upload{}=upload} ->
+        {:ok, upload}
       _ ->
-        msg = "'config_file' not specified."
-        halt_with_error(conn, 400, Poison.encode!(%{error: msg}))
+        {:error, :no_config}
     end
   end
 
   # Next we check to see that it's a config file
-  defp validate_file_format(conn, _) do
-    upload = conn.params["config_file"]
-
-    if Spanner.Config.config_file?(upload.filename) do
-      conn
+  defp validate_file_format(filename) do
+    if Config.config_file?(filename) do
+      :ok
     else
-      msg = ~s(Unsupported file format. Please upload a file in one of the following formats: #{Enum.join(Spanner.Config.config_extensions, ", ")})
-      halt_with_error(conn, 415, Poison.encode!(%{error: msg}))
+      {:error, :unsupported_format}
     end
   end
 
   # If all is good we parse the file and stick in conn.assigns
-  defp parse_config(conn, _) do
-    upload = conn.params["config_file"]
-
-    case Config.Parser.read_from_file(upload.path) do
+  defp parse_config(path) do
+    case Config.Parser.read_from_file(path) do
       {:ok, config} ->
-        assign(conn, :config, config)
+        {:ok, config}
       {:error, errors} ->
-        msg = "Unable to parse config file."
-        halt_with_error(conn, 400, Poison.encode!(%{error: msg, errors: errors}))
+        {:error, {:parse_error, errors}}
     end
   end
 
   # Before we can use the config we validate it's contents
-  defp validate_config(conn, _) do
-    case Config.validate(conn.assigns.config) do
+  defp validate_config(config) do
+    config_format_version = Map.get(config, "cog_bundle_version", :latest)
+    case Config.validate(config, config_format_version) do
       :ok ->
-        conn
+        :ok
       {:error, errors} ->
-        msg = "Invalid config file."
-        errors = Enum.map(errors, fn({msg, meta}) -> ~s(Error near #{meta}: #{msg}) end)
-        halt_with_error(conn, 400, Poison.encode!(%{error: msg, errors: errors}))
+        {:error, {:validation_error, errors}}
     end
+  end
+
+  # Create a new record in the DB for the deploy
+  defp persist(_config) do
+    :ok
+  end
+
+  # If all goes as planned, we can deploy the bundle
+  defp deploy(_config) do
+    {:ok, "WOOT! WOOT!"}
   end
 
   # Helper functions
 
-  # Halts and responds with with json
-  defp halt_with_error(conn, status, msg) do
+  defp send_success(conn, response) do
+    send_resp(conn, 201, response)
+  end
+
+  defp send_failure(conn, err) do
+    {status, msg} = error(err)
+
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(status, msg)
     |> halt
+  end
+
+  defp error(:no_config) do
+    {400, Poison.encode!(%{error: "'config_file' not specified."})}
+  end
+  defp error(:unsupported_format) do
+    msg = ~s(Unsupported file format. Please upload a file in one of the following formats: #{Enum.join(Spanner.Config.config_extensions, ", ")})
+    {415, Poison.encode!(%{error: msg})}
+  end
+  defp error({:parse_error, errors}) do
+    msg = "Unable to parse config file."
+    {422, Poison.encode!(%{error: msg, additional: errors})}
+  end
+  defp error({:validation_error, errors}) do
+    msg = "Invalid config file."
+    errors = Enum.map(errors, fn({msg, meta}) -> ~s(Error near #{meta}: #{msg}) end)
+    {422, Poison.encode!(%{error: msg, errors: errors})}
   end
 
 end
