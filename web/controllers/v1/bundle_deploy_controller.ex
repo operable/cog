ea487157d3742a125fea41644bdc31ca6819c530
Matthew Peck <matt@operable.io>
2016-04-04 15:52:24 -0400

    add bundle deploy controller

--- web/controllers/v1/bundle_deploy_controller.ex
+++ web/controllers/v1/bundle_deploy_controller.ex
@@ -2,6 +2,8 @@ defmodule Cog.V1.BundleDeployController do
   use Cog.Web, :controller
 
   alias Spanner.Config
+  alias Cog.Bundle.Install
+
 
   plug Cog.Plug.Authentication
   plug Cog.Plug.Authorization, permission: "#{Cog.embedded_bundle}:manage_commands"
@@ -61,7 +63,8 @@ defmodule Cog.V1.BundleDeployController do
   end
 
   # Create a new record in the DB for the deploy
-  defp persist(_config) do
+  defp persist(config) do
+    Install.install_bundle(config)
     {:ok, "WOOT! WOOT!"}
   end
 
