770c73309981d9fde2d05cc21fdee8bacc8d34ef
Matthew Peck <matt@operable.io>
2016-04-07 17:17:03 -0400

    update deps

--- mix.exs
+++ mix.exs
@@ -68,7 +68,7 @@ defmodule Cog.Mixfile do
      {:hedwig, "~> 0.3.0"},
      {:gproc, "~> 0.5.0", override: true},
      {:html_entities, "~> 0.3.0"},
-     {:spanner, github: "operable/spanner", ref: "34018db0e538cf6d4704df5c121ad66de5afcf3b"},
+     {:spanner, github: "operable/spanner", ref: "0c0fa93d394a740b518ce0db352675b4a8f7119f"},
      {:probe, github: "operable/probe", tag: "0.2"},
      {:exml, github: "paulgray/exml", tag: "2.2.1"},
      {:fumanchu, github: "operable/fumanchu"},
