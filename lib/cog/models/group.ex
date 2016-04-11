49b50bb1315fd3bf31045cb666e304227004fffb
Christopher Maier <chris@operable.io>
2016-04-11 10:10:57 -0400

    Include group name as a constraint

--- lib/cog/models/group.ex
+++ lib/cog/models/group.ex
@@ -36,6 +36,7 @@ defmodule Cog.Models.Group do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> unique_constraint(:name)
   end
 
 end
