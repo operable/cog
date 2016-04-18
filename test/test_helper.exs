51918a969263fdc629ad1648747a0f14b3ea3f64
vanstee <patrick@vanstee.me>
2016-04-18 16:08:02 -0500

    Replace slack token with fake token in cassettes

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,5 +3,6 @@ exclude_hipchat = System.get_env("TEST_HIPCHAT") == nil
 exclude_relay = System.get_env("TEST_RELAY") == nil
 
 ExVCR.Config.cassette_library_dir("test/fixtures/cassettes")
+ExVCR.Config.filter_sensitive_data("token=[^&]+", "token=xoxb-filtered-token")
 
 ExUnit.start(exclude: [slack: exclude_slack, hipchat: exclude_hipchat, relay: exclude_relay])
