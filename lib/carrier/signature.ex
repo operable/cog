9a653170f53a42ee6b0cff0ae500006d403b326c
Christopher Maier <chris@operable.io>
2016-04-28 11:28:55 -0400

    Delete dead Carrier code

--- lib/carrier/signature.ex
+++
@@ -1,9 +0,0 @@
-defprotocol Carrier.Signature do
-
-  @doc "Signs a JSON object using `Carrier.Credentials`"
-  def sign(creds, obj)
-
-  @doc "Verify JSON object signature"
-  def verify(creds, obj)
-
-end
