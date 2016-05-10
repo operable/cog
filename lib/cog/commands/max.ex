ba0a047652b049f001b8df7ed105130a6728e133
Matthew Peck <matt@operable.io>
2016-05-10 12:10:36 -0400

    update usage and add tests for max

--- lib/cog/commands/max.ex
+++ lib/cog/commands/max.ex
@@ -1,13 +1,20 @@
 defmodule Cog.Commands.Max do
   @moduledoc """
   This command allows the user to determine the maximum value given a
-  list of inputs.
+  list of inputs. The max value is based on erlangs term ordering.
 
-  ## Example
+  USAGE
+    max [ARGS ...]
 
-      @bot operable:max 49 9 2 2
-      @bot operable:max 0.48 0.2 1.8 3548.4 0.078
-      @bot operable:max "apple" "ball" "car" "zebra"
+  EXAMPLES
+    max 49 9 2 2
+    > 49
+
+    max 0.48 0.2 1.8 3548.4 0.078
+    > 3548.4
+
+    max "apple" "ball" "car" "zebra"
+    > zebra
   """
   use Cog.Command.GenCommand.Base, bundle: Cog.embedded_bundle
   require Logger
