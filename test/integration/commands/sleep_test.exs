f497acb8bff86f05e7ff3af75a6654c8e50f6513
Matthew Peck <matt@operable.io>
2016-05-10 16:46:19 -0400

    add sleep test

---
+++ test/integration/commands/sleep_test.exs
@@ -0,0 +1,18 @@
+defmodule Integration.Commands.SleepTest do
+  use Cog.AdapterCase, adapter: "test"
+
+  setup do
+    user = user("jfrost", first_name: "Jim", last_name: "Frost")
+    |> with_chat_handle_for("test")
+
+    {:ok, %{user: user}}
+  end
+
+  test "basic sleeping", %{user: user} do
+    response = send_message(user, ~s(@bot: sleep 1 | echo Lasagna is done cooking!))
+
+    [sleep] = decode_payload(response)
+
+    assert sleep.body == ["Lasagna is done cooking!"]
+  end
+end
