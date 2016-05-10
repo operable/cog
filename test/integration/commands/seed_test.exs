757d17058656b40a86224badf871a9626c633274
Matthew Peck <matt@operable.io>
2016-05-10 13:03:20 -0400

    update usage and add test for seed

---
+++ test/integration/commands/seed_test.exs
@@ -0,0 +1,20 @@
+defmodule Integration.Commands.SeedTest do
+  use Cog.AdapterCase, adapter: "test"
+
+
+  setup do
+    user = user("jfrost", first_name: "Jim", last_name: "Frost")
+    |> with_chat_handle_for("test")
+
+    {:ok, %{user: user}}
+  end
+
+  test "basic seeding", %{user: user} do
+    response = send_message(user, ~s(@bot: seed '[{"a": 1}, {"a": 3}, {"a": 2}]'))
+
+    json = decode_payload(response)
+
+    assert [%{a: 1}, %{a: 3}, %{a: 2}] = json
+  end
+
+end
