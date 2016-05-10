9eaa1bd02a4918129a1198efe628ba9119ea4ef6
Matthew Peck <matt@operable.io>
2016-05-10 13:38:41 -0400

    update usage and add a test for unique

---
+++ test/integration/commands/unique_test.exs
@@ -0,0 +1,18 @@
+defmodule Integration.Commands.UniqueTest do
+  use Cog.AdapterCase, adapter: "test"
+
+  setup do
+    user = user("jfrost", first_name: "Jim", last_name: "Frost")
+    |> with_chat_handle_for("test")
+
+    {:ok, %{user: user}}
+  end
+
+  test "basic uniquing", %{user: user} do
+    response = send_message(user, ~s(@bot: seed '[{"a": 1}, {"a": 3}, {"a": 1}]' | unique))
+
+    uniqued = decode_payload(response)
+
+    assert [%{a: 1}, %{a: 3}] == uniqued
+  end
+end
