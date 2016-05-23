d67dc48b8a1fba2da5a09d3d23fe05bb61374ea5
Christopher Maier <chris@operable.io>
2016-05-23 18:52:11 -0400

    Clean up bundle views

--- web/views/bundle_versions_view.ex
+++ web/views/bundle_versions_view.ex
@@ -6,15 +6,12 @@ defmodule Cog.V1.BundleVersionsView do
       name: bundle_version.bundle.name,
       version: to_string(bundle_version.version),
       permissions: render_many(bundle_version.permissions, Cog.V1.PermissionView, "permission.json", as: :permission),
-      commands: bundle_version |> command_names |> Enum.sort
-#      inserted_at: bundle_version.inserted_at,
-#      updated_at: bundle_version.updated_at
-     }
+      commands: bundle_version |> command_names |> Enum.sort,
+      inserted_at: bundle_version.inserted_at,
+      updated_at: bundle_version.updated_at}
   end
-
   def render("index.json", %{bundle_versions: bundle_versions}),
     do: %{bundle_versions: render_many(bundle_versions, __MODULE__, "bundle_version.json", as: :bundle_version)}
-
   def render("show.json", %{bundle_version: bundle_version, warnings: warnings}) when length(warnings) > 0 do
     warnings = Enum.map(warnings, fn({msg, meta}) -> ~s(Warning near #{meta}: #{msg}) end)
     %{warnings: warnings,
@@ -23,6 +20,8 @@ defmodule Cog.V1.BundleVersionsView do
   def render("show.json", %{bundle_version: bundle_version}) do
     %{bundle_version: render_one(bundle_version, __MODULE__, "bundle_version.json", as: :bundle_version)}
   end
+
+  ########################################################################
 
   defp command_names(bundle_version) do
     bundle_name = bundle_version.bundle.name
--- web/views/bundles_view.ex
+++ web/views/bundles_view.ex
@@ -2,25 +2,12 @@ defmodule Cog.V1.BundlesView do
   use Cog.Web, :view
 
   alias Cog.V1.RelayGroupView
-  # alias Cog.V1.CommandView
-  # alias Cog.V1.PermissionView
   alias Cog.V1.BundleVersionsView
-
-  defp ordered_version_strings(versions) do
-    versions
-    |> Enum.map(&(&1.version))
-    |> Enum.sort
-    |> Enum.reverse
-    |> Enum.map(&to_string/1)
-  end
 
   def render("bundle.json", %{bundle: bundle}=_params) do
     enabled_version = Map.fetch!(bundle, :enabled_version)
     enabled_version = if Ecto.assoc_loaded?(enabled_version) do
-      %{enabled_version: render_one(enabled_version,
-                                    BundleVersionsView,
-                                    "bundle_version.json",
-                                    as: :bundle_version)}
+      %{enabled_version: render_one(enabled_version, BundleVersionsView, "bundle_version.json", as: :bundle_version)}
     else
       %{}
     end
@@ -39,65 +26,21 @@ defmodule Cog.V1.BundlesView do
     |> Map.merge(enabled_version)
     |> Map.merge(relay_groups)
   end
-
-  def render("index.json", %{bundles: bundles}=assigns) do
-    %{bundles: render_many(bundles, __MODULE__,
-                           "bundle.json",
-                           Map.put(assigns, :as, :bundle))}
+  def render("index.json", %{bundles: bundles}) do
+    %{bundles: render_many(bundles, __MODULE__, "bundle.json", as: :bundle)}
+  end
+  def render("show.json", %{bundle: bundle}) do
+    %{bundle: render_one(bundle, __MODULE__, "bundle.json", as: :bundle)}
   end
 
-  def render("show.json", %{bundle: bundle, warnings: warnings}=assigns)
-  when length(warnings) > 0 do
-    warnings = Enum.map(warnings, fn({msg, meta}) -> ~s(Warning near #{meta}: #{msg}) end)
-    %{warnings: warnings,
-      bundle: render_one(bundle, __MODULE__, "bundle.json",
-                         Map.merge(assigns,
-                                   %{as: :bundle,
-                                     include: [:commands, :relay_groups, :namespace]}))}
-  end
-  def render("show.json", %{bundle: bundle}=assigns) do
-    %{bundle: render_one(bundle, __MODULE__,
-                         "bundle.json",
-                         Map.merge(assigns,
-                                   %{as: :bundle,
-                                     include: [:commands, :relay_groups, :namespace]}))}
+  ########################################################################
+
+  defp ordered_version_strings(versions) do
+    versions
+    |> Enum.map(&(&1.version))
+    |> Enum.sort
+    |> Enum.reverse
+    |> Enum.map(&to_string/1)
   end
 
-  # defp render_includes(inc_fields, resource) do
-  #   Map.get(inc_fields, :include, [])
-  #   |> Enum.reduce(%{}, fn(field, reply) ->
-  #     case render_include(field, resource) do
-  #       nil -> reply
-  #       {key, value} -> Map.put(reply, key, value)
-  #     end
-  #   end)
-  # end
-
-  # defp render_include(:commands, bundle) do
-  #   value = Map.fetch!(bundle, :commands)
-  #   case Ecto.assoc_loaded?(value) do
-  #     true ->
-  #       {:commands, render_many(value, CommandView, "command.json", as: :command, include: [:rules])}
-  #     false ->
-  #       nil
-  #   end
-  # end
-  # defp render_include(:relay_groups, bundle) do
-  #   value = Map.fetch!(bundle, :relay_groups)
-  #   case Ecto.assoc_loaded?(value) do
-  #     true ->
-  #       {:relay_groups, render_many(value, RelayGroupView, "relay_group.json", as: :relay_group)}
-  #     false ->
-  #       nil
-  #   end
-  # end
-  # defp render_include(:namespace, bundle) do
-  #   namespace = Map.fetch!(bundle, :namespace)
-  #   case Ecto.assoc_loaded?(namespace) do
-  #     true ->
-  #       {:permissions, render_many(namespace.permissions, PermissionView, "permission.json", as: :permission, include: [:namespace])}
-  #     false ->
-  #       nil
-  #   end
-  # end
 end
