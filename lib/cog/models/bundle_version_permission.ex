5dd223d7eff3897da004e1ba7466fb6520b2ffe4
Christopher Maier <chris@operable.io>
2016-05-23 06:59:32 -0400

    Fix up permission linkage
    We now expose the permissions of a bundle version as an Ecto association
    on the BundleVersion model. This also exposed a bug in how we were
    linking permissions to bundle versions: previously, we ended up linking
    _all_ permissions from a bundle to the bundle version, instead of just
    the permissions present in _that_ specific version. That's now fixed.

---
+++ lib/cog/models/bundle_version_permission.ex
@@ -0,0 +1,12 @@
+defmodule Cog.Models.PermissionBundleVersion do
+  use Cog.Model
+
+  @primary_key false
+  schema "permission_bundle_version_v2" do
+    belongs_to :permission, Cog.Models.Permission, references: :id
+    belongs_to :bundle_version, Cog.Models.BundleVersion, references: :id
+  end
+
+  # Insertions are handled via JoinTable, so nothing else is needed
+  # for this model
+end
