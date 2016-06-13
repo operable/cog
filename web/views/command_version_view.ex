28a135de1d63f90fbe84ecbcadc8d006a2c7b8f7
vanstee <patrick@vanstee.me>
2016-06-13 09:21:13 -0600

    Preload commands and their rules

--- web/views/command_version_view.ex
+++ web/views/command_version_view.ex
@@ -2,11 +2,18 @@ defmodule Cog.V1.CommandVersionView do
   use Cog.Web, :view
 
   def render("command_version.json", %{command_version: command_version}) do
-    %{id: command_version.id,
-      bundle: command_version.command.bundle.name,
-      name: command_version.command.name,
-      description: command_version.description,
-      documentation: command_version.documentation}
+    attrs = %{id: command_version.id,
+              bundle: command_version.command.bundle.name,
+              name: command_version.command.name,
+              description: command_version.description,
+              documentation: command_version.documentation}
+
+    case Ecto.assoc_loaded?(command_version.command.rules) do
+      true ->
+        Map.put(attrs, :rules, render_many(command_version.command.rules, Cog.V1.RuleView, "rule.json"))
+      false ->
+        attrs
+    end
   end
 
 end
