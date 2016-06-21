1c72f6b0d3402cb1e15e82e5e7a69e4860b0614e
Christopher Maier <chris@operable.io>
2016-06-21 12:40:41 -0400

    Add 'attach-handle' and 'detach-handle' subcommands to `operable:user`
    This allows users to link (and unlink) chat handles with Cog accounts directly in chat.
    
    Fixes #787

---
+++ lib/cog/commands/user/attach_handle.ex
@@ -0,0 +1,45 @@
+defmodule Cog.Commands.User.AttachHandle do
+  alias Cog.Repository.Users
+  alias Cog.Repository.ChatHandles
+
+  require Cog.Commands.Helpers, as: Helpers
+
+  Helpers.usage """
+  Attach a chat handle for this chat provider with an existing Cog user.
+
+  USAGE
+    user attach-handle [FLAGS] <username> <handle>
+
+  ARGS
+    username   The name of a user
+    handle     The user's chat handle
+
+  FLAGS
+    -h, --help  Display this usage info
+  """
+
+  def attach(%{options: %{"help" => true}}, _args) do
+    show_usage
+  end
+  def attach(_req, [user_name, handle]) do
+    case Users.by_username(user_name) do
+      {:error, :not_found} ->
+        {:error, {:resource_not_found, "user", user_name}}
+      {:ok, user} ->
+        {:ok, adapter} = Cog.chat_adapter_module
+        case ChatHandles.set_handle(user, adapter.name, handle) do
+          {:ok, handle} ->
+            {:ok, "user-attach-handle", Cog.V1.ChatHandleView.render("show.json", %{chat_handle: handle})}
+          {:error, error} ->
+            {:error, error}
+        end
+    end
+  end
+  def attach(_, []),
+    do: {:error, {:not_enough_args, 2}}
+  def attach(_, [_]),
+    do: {:error, {:not_enough_args, 2}}
+  def attach(_, _),
+    do: {:error, {:too_many_args, 2}}
+
+end
---
+++ test/integration/commands/user_test.exs
@@ -0,0 +1,108 @@
+defmodule Integration.Commands.UserTest do
+  use Cog.AdapterCase, adapter: "test"
+  alias Cog.Repository.Users
+  alias Cog.Support.ModelUtilities
+
+  setup do
+    user = user("tester")
+    |> with_chat_handle_for("test")
+    |> with_permission("operable:manage_users")
+
+    {:ok, %{user: user}}
+  end
+
+  test "listing users", %{user: user} do
+    payload = user
+    |> send_message("@bot: operable:user list")
+    |> decode_payload
+    |> Enum.sort_by(fn(b) -> b[:username] end)
+
+    assert [%{username: "admin"},
+            %{username: "tester"}] = payload
+  end
+
+  test "information about a specific user", %{user: user} do
+    [payload] = user
+    |> send_message("@bot: operable:user info admin")
+    |> decode_payload
+
+    assert %{username: "admin"} = payload
+  end
+
+  test "information about a missing user fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user info not-a-real-user")
+    assert_error_message_contains(response, "Could not find 'user' with the name 'not-a-real-user'")
+  end
+
+  test "not providing the user to get information about fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user info")
+    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 1.")
+  end
+
+  test "providing multiple users to get information about fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user info admin tester")
+    assert_error_message_contains(response, "Too many args. Arguments required: exactly 1.")
+  end
+
+  test "attaching a chat handle to a user works", %{user: user} do
+    user("dummy")
+
+    [payload] = user
+    |> send_message("@bot: operable:user attach-handle dummy dummy-handle")
+    |> decode_payload
+
+    assert %{chat_provider: %{name: "test"},
+             handle: "dummy-handle",
+             id: _,
+             username: "dummy"} = payload
+  end
+
+  test "attaching a chat handle to a non-existent user fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user attach-handle not-a-user test-handle")
+    assert_error_message_contains(response, "Could not find 'user' with the name 'not-a-user'")
+  end
+
+  test "attaching a chat handle without specifying a chat handle fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user attach-handle dummy")
+    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 2")
+  end
+
+  test "attaching a chat handle without specifying a user or chat handle fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user attach-handle")
+    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 2")
+  end
+
+  test "detaching a chat handle works", %{user: user} do
+    user("dummy")
+    |> with_chat_handle_for("test")
+
+    [payload] = user
+    |> send_message("@bot: operable:user detach-handle dummy")
+    |> decode_payload
+
+    assert %{chat_provider: %{name: "test"},
+             username: "dummy"} = payload
+  end
+
+  test "detaching a chat handle works even if there wasn't a handle to begin with", %{user: user} do
+    user("dummy")
+
+    [payload] = user
+    |> send_message("@bot: operable:user detach-handle dummy")
+    |> decode_payload
+
+    assert %{chat_provider: %{name: "test"},
+             username: "dummy"} = payload
+  end
+
+  test "detaching a chat handle from a non-existent user fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user detach-handle not-a-user")
+    assert_error_message_contains(response, "Could not find 'user' with the name 'not-a-user'")
+  end
+
+  test "detaching a chat handle without specifying a user fails", %{user: user} do
+    response = send_message(user, "@bot: operable:user detach-handle")
+    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 1")
+  end
+
+end
