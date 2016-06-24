723e779fac69ade40f2aa2a2ba8f9ea553f84bb5
Matthew Peck <matt@operable.io>
2016-06-23 22:29:34 -0400

    tweaks

--- web/plugs/authorization.ex
+++ web/plugs/authorization.ex
@@ -42,11 +42,13 @@ defmodule Cog.Plug.Authorization do
       raise ":permission key must be a string, but was '#{inspect permission}' instead"
     end
     {_,_} = Permission.split_name(permission) # error if can't be split
-    self_updates = Keyword.get(opts, :allow_self_updates, [])
-    unless is_list(self_updates) do
-      raise ":allow_self_updates must be a list, but was '#{inspect self_updates}' instead"
+    self_updates = Keyword.get(opts, :self_updates_on, [])
+    |> List.wrap
+
+    unless Enum.all?(self_updates, &is_atom/1) do
+      raise ":allow_self_updates must be a list of atoms, but was '#{inspect self_updates}' instead"
     end
-    opts
+    Keyword.put(opts, :self_updates_on, self_updates)
   end
 
   # Expects to be called after the `Cog.Plug.Authentication` plug
@@ -75,7 +77,7 @@ defmodule Cog.Plug.Authorization do
   # in the future. For example, this would allow users to update
   # related data with paths such as `/v1/users/:user_id/profiles/:id`.
   defp self_updating?(opts, conn) do
-    case Keyword.get(opts, :allow_self_updates, []) do
+    case Keyword.get(opts, :self_updates_on) do
       [] -> false
       actions ->
         conn.private.phoenix_action in actions and
