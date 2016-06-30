56e7c2b88bdb1a0ce848de72696f2c428781a550
vanstee <patrick@vanstee.me>
2016-06-30 09:07:24 -0400

    Update slack now that changes are upstream

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Cog.Mixfile do
   end
 
   defp deps do
-    [{:slack, github: "operable/Elixir-Slack", branch: "fix-unsafe-var-warning"},
+    [{:slack, github: "BlakeWilliams/Elixir-Slack"},
      {:websocket_client, github: "jeremyong/websocket_client"},
      {:poison, "~> 1.5.2"},
      {:ibrowse, "~> 4.2.2", override: true},
