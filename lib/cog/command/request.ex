bf48f563e645b1ce9aa18fbe49fb74bfa9347553
Kevin Smith <kevin@operable.io>
2016-06-30 16:01:29 -0400

    Incorporating code review feedback

--- lib/cog/command/request.ex
+++ lib/cog/command/request.ex
@@ -4,18 +4,16 @@ defmodule Cog.Command.Request do
 
   require Logger
 
-  # defmarshalled [:room, :requestor, :user, :command, :args, :options,
-  #                :command_config, :reply_to, :cog_env, :invocation_id,
-  #                :invocation_step, :service_token, :services_root]
-
   defmarshalled [:options, :args, :cog_env, :invocation_step]
 
   defp validate(request) do
-    cond do
+    case do
       request.invocation_step == nil ->
         {:error, {:empty_field, :invocation_step}}
       request.cog_env == nil ->
         {:error, {:empty_field, :cog_env}}
+      true ->
+        request
     end
   end
 
