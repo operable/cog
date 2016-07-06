ee0da11a3009db8d658a98acdc7653e5a6ff4159
Kevin Smith <kevin@operable.io>
2016-07-06 10:32:53 -0400

    Introduce env var to configure self-registration

--- config/config.exs
+++ config/config.exs
@@ -65,7 +65,7 @@ config :cog, :template_cache_ttl, {60, :sec}
 config :cog, :user_perms_ttl, {10, :sec}
 
 # Enable/disable user self-registration
-config :cog, :self_registration, true
+config :cog, :self_registration, System.get_env("COG_ALLOW_SELF_REGISTRATION") != nil || false
 
 config :cog, :emqttc,
   log_level: :info
