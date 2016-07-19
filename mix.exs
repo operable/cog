b1e0a498553b78f11fbdea9c848eef8dcac830f2
Christopher Maier <chris@operable.io>
2016-07-19 14:00:08 -0400

    Require Elixir >= 1.3.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Cog.Mixfile do
   def project do
     [app: :cog,
      version: "0.12.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3.1",
      erlc_paths: ["emqttd_plugins"],
      erlc_options: [:debug_info, :warnings_as_errors],
      elixirc_options: [warnings_as_errors: System.get_env("ALLOW_WARNINGS") == nil],
