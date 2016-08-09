68249a52b3aa0e251ff7adf1e5b3a0d7c2e6bd57
vanstee <patrick@vanstee.me>
2016-08-09 15:38:16 -0400

    Simplify and future-proof migration by using raw sql

--- priv/repo/migrations/20160803181844_replace_username_with_slack_handle.exs
+++ priv/repo/migrations/20160803181844_replace_username_with_slack_handle.exs
@@ -1,35 +1,15 @@
 defmodule Cog.Repo.Migrations.ReplaceUsernameWithSlackHandle do
   use Ecto.Migration
-  alias Cog.Repo
-  alias Cog.Models.User
 
   def change do
-    users = User
-    |> Repo.all
-    |> Repo.preload([chat_handles: :chat_provider])
-
-    Enum.each(users, fn user ->
-      username_is_email = user.username == user.email_address
-
-      slack_handle = Enum.find_value(user.chat_handles, fn chat_handle ->
-        case chat_handle.chat_provider.name do
-          "slack" ->
-            chat_handle.handle
-          _ ->
-            nil
-        end
-      end)
-
-      slack_handle = slack_handle || user.username
-
-      params = case username_is_email do
-        false ->
-          %{}
-        true ->
-          %{"username" => slack_handle}
-      end
-
-      Repo.update!(User.changeset(user, params))
-    end)
+    execute """
+    UPDATE users
+    SET username = chat_handles.handle
+    FROM chat_handles, chat_providers
+    WHERE users.username = users.email_address
+      AND users.id = chat_handles.user_id
+      AND chat_handles.provider_id = chat_providers.id
+      AND chat_providers.name = 'slack';
+    """
   end
 end
