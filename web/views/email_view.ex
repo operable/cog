47d25738314efd1ab3a8d747b0a7c5668f78d885
Matthew Peck <matt@operable.io>
2016-08-09 11:45:10 -0400

    rework how reset_url is generated

--- web/views/email_view.ex
+++ web/views/email_view.ex
@@ -7,19 +7,27 @@ defmodule Cog.EmailView do
       nil ->
         Logger.warn("""
         Base URL not set for password reset, defaulting to 'localhost'.
-        The base url is the url Cog sends to users when a password reset is requested.
+        The base url is used to generate a password reset url when a request
+        to reset a password is received. The password reset url is generated
+        by appending a token to the base url.
 
         Please set the env var 'COG_PASSWORD_RESET_BASE_URL'.
         """)
 
-        "localhost"
+        %URI{host: "localhost"}
 
       base_url ->
-        base_url
+        URI.parse(base_url)
     end
 
-    URI.parse(base_url)
-    |> Map.put(:query, "token=#{token}")
+    %{base_url | path: build_path(base_url.path, token)}
     |> URI.to_string
+  end
+
+  defp build_path(nil, token),
+    do: "/#{token}"
+  defp build_path(path, token) do
+    String.split(path, "/") ++ [token]
+    |> Enum.join("/")
   end
 end
