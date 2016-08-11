a99ae372aa6814625b2c17e62f68f63bec831a74
Matthew Peck <matt@operable.io>
2016-08-11 10:40:08 -0400

    update email test

--- test/views/email_view.exs
+++ test/views/email_view.exs
@@ -9,4 +9,13 @@ defmodule Cog.EmailViewTest do
     assert url == "http://example.com:4000/reset-password/aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"
   end
 
+  test "reset_url is not shown if password_reset_base_url is not set" do
+    Application.delete_env(:cog, :password_reset_base_url)
+
+    assigns = [token: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"]
+    email = Phoenix.View.render_to_string(Cog.EmailView, "reset_password.text", assigns)
+
+    refute email =~ "reset your password using the webui"
+  end
+
 end
