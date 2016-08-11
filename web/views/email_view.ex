c315ca4b84666a0d7c8f2e545822fa2fad5617b7
Matthew Peck <matt@operable.io>
2016-08-11 11:30:34 -0400

    add doc to webui? fun

--- web/views/email_view.ex
+++ web/views/email_view.ex
@@ -2,6 +2,11 @@ defmodule Cog.EmailView do
   use Cog.Web, :view
   require Logger
 
+  @doc """
+  Returns true if the password_reset_base_url var is set. password_reset_base_url
+  is the url of the external web application utilizing the password reset endpoints
+  of Cog. Internally this is the flywheel password reset url.
+  """
   def webui?(),
     do: not(is_nil(Application.get_env(:cog, :password_reset_base_url)))
 
