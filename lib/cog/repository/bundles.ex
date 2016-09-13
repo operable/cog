d5a649a629c309a0ca1adb52128ca6247b49eba7
vanstee <patrick@vanstee.me>
2016-09-13 11:29:26 -0400

    Only return disabled bundles from disabled bundle query

--- lib/cog/repository/bundles.ex
+++ lib/cog/repository/bundles.ex
@@ -303,7 +303,7 @@ defmodule Cog.Repository.Bundles do
   def highest_disabled_versions do
     query = from bv in BundleVersion,
             left_join: e in "enabled_bundle_versions",
-              on: bv.bundle_id == e.bundle_id and bv.version == e.version,
+              on: bv.bundle_id == e.bundle_id,
             join: b in assoc(bv, :bundle),
             where: is_nil(e.bundle_id) and b.name != "site",
             distinct: bv.bundle_id,
