ba4e41d54a560cdfbbdab6175acdfa351d5d60b7
Kevin Smith <kevin@operable.io>
2016-09-15 15:42:04 -0400

    Locking deps to release branches

--- mix.exs
+++ mix.exs
@@ -61,12 +61,12 @@ defmodule Cog.Mixfile do
     [
       # Operable Code
       ########################################################################
-      {:adz, github: "operable/adz"},
-      {:conduit, github: "operable/conduit"},
+      {:adz, github: "operable/adz", branch: "v0.14.0"},
+      {:conduit, github: "operable/conduit", branch: "v0.14.0"},
       {:fumanchu, github: "operable/fumanchu"},
-      {:greenbar, github: "operable/greenbar"},
-      {:probe, github: "operable/probe"},
-      {:spanner, github: "operable/spanner"},
+      {:greenbar, github: "operable/greenbar", branch: "v0.14.0"},
+      {:probe, github: "operable/probe", branch: "v0.14.0"},
+      {:spanner, github: "operable/spanner", branch: "v0.14.0"},
 
       # MQTT-related
       ########################################################################
