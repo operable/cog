4ca19c3862f7151904a6d7d316059c3600117c3c
vanstee <patrick@vanstee.me>
2016-09-26 17:06:20 -0400

    Add a test that renders the bundle-install template

---
+++ test/cog/chat/slack/templates/embedded/bundle_install_test.exs
@@ -0,0 +1,15 @@
+defmodule Cog.Chat.Slack.Templates.Embedded.BundleInstallTest do
+  use Cog.TemplateCase
+
+  test "bundle-info template" do
+    data = %{"results" => [%{"name" => "heroku",
+                             "version" => "0.0.4"}]}
+
+    expected = """
+    Bundle "heroku" version "0.0.4" installed.
+    """ |> String.strip
+
+    assert_rendered_template(:slack, :embedded, "bundle-install", data, expected)
+  end
+
+end
