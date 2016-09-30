620801156622b575528cdf0c4bbd02963a4c60a9
Kevin Smith <kevin@operable.io>
2016-09-30 15:04:09 -0400

    Test fixups after rebase

---
+++ test/cog/chat/hipchat/templates/embedded/help_bundles_test.exs
@@ -0,0 +1,16 @@
+defmodule Cog.Chat.HipChat.Templates.Embedded.HelpBundlesTest do
+  use Cog.TemplateCase
+
+  test "help-bundles template" do
+    data = %{"results" => [%{"enabled" => [%{"name" => "operable"}],
+                             "disabled" => [%{"name" => "test-bundle"}]}]}
+
+    expected = "<strong>Enabled Bundles</strong><br/><br/>" <>
+      "<ul><li>operable</li></ul><br/>" <>
+      "<strong>Disabled Bundles</strong><br/><br/>" <>
+      "<ul><li>test-bundle</li></ul><br/>" <>
+      "To learn more about a specific bundle and the commands available within it, you can use \"operable:help <bundle>\"."
+
+    assert_rendered_template(:hipchat, :embedded, "help-bundles", data, expected)
+  end
+end
--- test/cog/chat/hipchat/templates/embedded/help_command_test.exs
+++ test/cog/chat/hipchat/templates/embedded/help_command_test.exs
@@ -2,13 +2,18 @@ defmodule Cog.Chat.HipChat.Templates.Embedded.HelpCommandTest do
   use Cog.TemplateCase
 
   test "help-command template" do
-    data = %{"results" => [%{"documentation" => "big ol' doc string"}]}
-    expected = "<pre>big ol' doc string\n</pre>"
+    data = %{"results" => [%{"name" => "test",
+                             "description" => "Do a test thing",
+                             "synopsis" => "test --do-a-thing",
+                             "bundle" => %{"author" => "vanstee"}}]}
+    expected = "<strong>Name</strong><br/><br/>" <>
+      "test - Do a test thing<br/><br/>" <>
+      "<strong>Synopsis</strong><br/><br/>" <>
+      "<pre>test --do-a-thing\n</pre><br/>" <>
+      "<strong>Author</strong><br/><br/>" <>
+      "vanstee<br/>"
 
     assert_rendered_template(:hipchat, :embedded, "help-command", data, expected)
   end
-
-  # TODO: Can a command ever not have documentation? If so, the
-  # template should handle that.
 
 end
