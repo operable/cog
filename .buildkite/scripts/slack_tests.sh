11c781c0b35d3699bab806fbe1d7313335787df6
Matthew Peck <matt@operable.io>
2016-10-20 17:32:48 -0400

    use the existing wait-for-it script

--- .buildkite/scripts/slack_tests.sh
+++ .buildkite/scripts/slack_tests.sh
@@ -2,6 +2,14 @@
 
 set -eu
 
-.buildkite/scripts/wait_for_postgres.sh && \
-  MIX_ENV=test mix do ecto.reset --no-start, test --only=slack
+export MIX_ENV="test"
+
+echo "Waiting for Postgres to become available..."
+./scripts/wait-for-it.sh -s -t 0 -h "postgres" -p "5432" && true
+
+echo "Apply database migrations..."
+mix ecto.migrate --no-deps-check
+
+echo "Running Slack tests..."
+mix test --only=slack
 
