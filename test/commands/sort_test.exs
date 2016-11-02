c2388584c22ab128ff94501d39e926831babc483
Matthew Peck <matt@operable.io>
2016-11-02 15:36:45 -0400

    sort test

--- test/commands/sort_test.exs
+++ test/commands/sort_test.exs
@@ -1,27 +1,49 @@
 defmodule Cog.Test.Commands.SortTest do
-  use Cog.AdapterCase, adapter: "test"
-
-  @moduletag :skip
+  use Cog.CommandCase, command_module: Cog.Commands.Sort
 
   setup do
-    user = user("jfrost", first_name: "Jim", last_name: "Frost")
-    |> with_chat_handle_for("test")
+    seed = [%{"a" => 1},
+            %{"a" => 3},
+            %{"a" => 2}]
 
-    {:ok, %{user: user}}
+    {:ok, %{seed: seed}}
   end
 
-  test "basic sorting", %{user: user} do
-    response = send_message(user, ~s(@bot: seed '[{"a": 1}, {"a": 3}, {"a": 2}]' | sort))
-    assert response == [%{a: 1}, %{a: 2}, %{a: 3}]
+  test "basic sorting", %{seed: seed} do
+    request = Enum.map(seed, &(new_req(cog_env: &1)))
+
+    # When you send a list of requests you get a list of responses back
+    # We only care about the last one though, the rest should be nil
+    {:ok, response} = send_req(request) |> List.last
+
+    decoded = Poison.decode!(response)
+    assert [%{"a" => 1},
+            %{"a" => 2},
+            %{"a" => 3}] = decoded
   end
 
-  test "sorting in descending order", %{user: user} do
-    response = send_message(user, ~s(@bot: seed '[{"a": 1}, {"a": 3}, {"a": 2}]' | sort --desc))
-    assert response == [%{a: 3}, %{a: 2}, %{a: 1}]
+  test "sorting in descending order", %{seed: seed} do
+    request = Enum.map(seed, &(new_req(cog_env: &1, options: %{"desc" => true})))
+
+    {:ok, response} = send_req(request) |> List.last
+
+    decoded = Poison.decode!(response)
+    assert [%{"a" => 3},
+            %{"a" => 2},
+            %{"a" => 1}] = decoded
   end
 
-  test "sorting by specific fields", %{user: user} do
-    response = send_message(user, ~s(@bot: seed '[{"a": 3, "b": 4}, {"a": 1, "b": 4}, {"a": 2, "b": 6}]' | sort b a))
-    assert response == [%{a: 1, b: 4}, %{a: 3, b: 4}, %{a: 2, b: 6}]
+  test "sorting by specific fields" do
+    seed = [%{"a" => 3, "b" => 4},
+            %{"a" => 1, "b" => 4},
+            %{"a" => 2, "b" => 6}]
+    request = Enum.map(seed, &(new_req(cog_env: &1, args: ["b", "a"])))
+
+    {:ok, response} = send_req(request) |> List.last
+
+    decoded = Poison.decode!(response)
+    assert [%{"a" => 1, "b" => 4},
+            %{"a" => 3, "b" => 4},
+            %{"a" => 2, "b" => 6}] = decoded
   end
 end
--- test/support/command_case.ex
+++ test/support/command_case.ex
@@ -7,7 +7,7 @@ defmodule Cog.CommandCase do
   use ExUnit.CaseTemplate
 
   using command_module: command_module do
-    quote do
+    quote location: :keep do
       alias Cog.Support.ModelUtilities
       alias unquote(command_module)
 
@@ -23,19 +23,41 @@ defmodule Cog.CommandCase do
                               reply_to: Keyword.get(opts, :reply_to, "fake_reply_to"),
                               requestor: Keyword.get(opts, :requestor, %Cog.Chat.User{}),
                               room: Keyword.get(opts, :room, %Cog.Chat.Room{}),
-                              service_token: Keyword.get(opts, :service_token, "fake_service_token"),
-                              services_root: Keyword.get(opts, :services_root, "fake_services_root"),
+                              service_token: Keyword.get(opts, :service_token, service_token()),
+                              services_root: Keyword.get(opts, :services_root, Cog.ServiceEndpoint.public_url()),
                               user: Keyword.get(opts, :user, %{})}
       end
 
-      defp send_req(module \\ unquote(command_module), %Cog.Messages.Command{}=req) do
+      defp send_req(module \\ unquote(command_module), req)
+      defp send_req(module, %Cog.Messages.Command{}=req) do
         case module.handle_message(req, %{}) do
+          {:reply, _reply_to, _template, nil, _state} ->
+            {:ok, nil}
+          {:reply, _reply_to, nil, _state} ->
+            {:ok, nil}
           {:reply, _reply_to, _template, reply, _state} ->
             {:ok, Poison.encode!(reply)}
           {:reply, _reply_to, reply, _state} ->
             {:ok, Poison.encode!(reply)}
           {:error, _reply_to, error_message, _state} ->
             {:error, error_message}
+        end
+      end
+      defp send_req(module, reqs) when is_list(reqs) do
+        Enum.map(reqs, &(%{&1 | invocation_step: nil}))
+        |> List.update_at(0, &(%{&1 | invocation_step: "first"}))
+        |> List.update_at(-1, &(%{&1 | invocation_step: "last"}))
+        |> Enum.map(&(send_req(module, &1)))
+      end
+
+      defp service_token do
+        case Process.get(:service_token) do
+          nil ->
+            token = Cog.Command.Service.Tokens.new()
+            Process.put(:service_token, token)
+            token
+          token ->
+            token
         end
       end
 
