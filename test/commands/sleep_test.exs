1a513d4f064c5b6b08cd4c9028e8d2b8c9cb3fac
Matthew Peck <matt@operable.io>
2016-11-05 18:37:20 -0400

    sleep test updates

--- test/commands/sleep_test.exs
+++ test/commands/sleep_test.exs
@@ -1,17 +1,51 @@
 defmodule Cog.Test.Commands.SleepTest do
-  use Cog.AdapterCase, adapter: "test"
+  use Cog.CommandCase, command_module: Cog.Commands.Sleep
 
-  @moduletag :skip
+  test "basic sleeping" do
+    request = fn ->
+      new_req(cog_env: "foo", args: [3])
+      |> send_req()
+    end
 
-  setup do
-    user = user("jfrost", first_name: "Jim", last_name: "Frost")
-    |> with_chat_handle_for("test")
+    # We're running the command as a task so it doesn't block the test process
+    task = Task.async(request)
 
-    {:ok, %{user: user}}
+    # First we yield with a short timeout to ensure that the command is sleeping
+    assert(nil == Task.yield(task, 1000))
+
+    # Then we yield again with a longer timeout to make sure something is returned
+    # and the command didn't just crash.
+    {:ok, task_response} = Task.yield(task, 3000)
+    assert({:ok, ["foo"]} == task_response)
   end
 
-  test "basic sleeping", %{user: user} do
-    response = send_message(user, ~s(@bot: sleep 1 | echo Lasagna is done cooking!))
-    assert response == "Lasagna is done cooking!"
+  test "should NOT sleep if it's NOT the last invocation step" do
+
+    Enum.each(["first", nil], fn(step) ->
+      request = fn ->
+        new_req(args: [5], invocation_step: step, invocation_id: "dont_sleep")
+        |> send_req()
+      end
+
+      # We're running the command as a task so it doesn't block the test process
+      response = Task.async(request)
+      |> Task.yield(1000) # The task should not sleep, but in case it does, we'll set the timeout
+
+      # The task should return before the timeout, so we shouldn't get nil this time
+      refute(is_nil(response), "Slept on the #{step} invocation step.")
+    end)
+
   end
+
+  test "should accumulate data" do
+    results = Enum.map(["first", nil, "last"], fn(step) ->
+      new_req(cog_env: "foo", args: [0], invocation_step: step, invocation_id: "sleep_accum")
+      |> send_req()
+    end)
+
+    assert([{:ok, nil},
+            {:ok, nil},
+            {:ok, ["foo", "foo", "foo"]}] == results)
+  end
+
 end
