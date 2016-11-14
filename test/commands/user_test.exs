2681c426b110e5609728b8d06fee71738e2e537f
Matthew Peck <matt@operable.io>
2016-11-14 17:02:10 -0500

    user test updates

--- test/commands/user_test.exs
+++ test/commands/user_test.exs
@@ -1,52 +1,59 @@
 defmodule Cog.Test.Commands.UserTest do
-  use Cog.AdapterCase, adapter: "test"
+  use Cog.CommandCase, command_module: Cog.Commands.User
 
-  @moduletag :skip
+  import Cog.Support.ModelUtilities, only: [user: 1,
+                                            with_chat_handle_for: 2]
 
-  setup do
-    user = user("tester")
-    |> with_chat_handle_for("test")
-    |> with_permission("operable:manage_users")
+  setup :with_users
 
-    {:ok, %{user: user}}
-  end
-
-  test "listing users", %{user: user} do
-    payload = user
-    |> send_message("@bot: operable:user list")
-    |> Enum.sort_by(fn(b) -> b[:username] end)
+  test "listing users" do
+    payload = new_req(args: ["list"])
+              |> send_req()
+              |> unwrap()
+              |> Enum.sort_by(fn(b) -> b[:username] end)
 
     assert [%{username: "admin"},
             %{username: "tester"}] = payload
   end
 
-  test "information about a specific user", %{user: user} do
-    [payload] = user
-    |> send_message("@bot: operable:user info admin")
+  test "information about a specific user" do
+    payload = new_req(args: ["info", "admin"])
+              |> send_req()
+              |> unwrap()
 
     assert %{username: "admin"} = payload
   end
 
-  test "information about a missing user fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user info not-a-real-user")
-    assert_error_message_contains(response, "Could not find 'user' with the name 'not-a-real-user'")
+  test "information about a missing user fails" do
+    error = new_req(args: ["info", "not-a-real-user"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Could not find 'user' with the name 'not-a-real-user'")
   end
 
-  test "not providing the user to get information about fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user info")
-    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 1.")
+  test "not providing the user to get information about fails" do
+    error = new_req(args: ["info"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Not enough args. Arguments required: exactly 1.")
   end
 
-  test "providing multiple users to get information about fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user info admin tester")
-    assert_error_message_contains(response, "Too many args. Arguments required: exactly 1.")
+  test "providing multiple users to get information about fails" do
+    error = new_req(args: ["info", "admin", "tester"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Too many args. Arguments required: exactly 1.")
   end
 
-  test "attaching a chat handle to a user works", %{user: user} do
+  test "attaching a chat handle to a user works" do
     user("dummy")
 
-    [payload] = user
-    |> send_message("@bot: operable:user attach-handle dummy dummy-handle")
+    payload = new_req(args: ["attach-handle", "dummy", "dummy-handle"])
+              |> send_req()
+              |> unwrap()
 
     assert %{chat_provider: %{name: "test"},
              handle: "dummy-handle",
@@ -54,69 +61,95 @@ defmodule Cog.Test.Commands.UserTest do
              username: "dummy"} = payload
   end
 
-  test "attaching a chat handle to a non-existent user fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user attach-handle not-a-user test-handle")
-    assert_error_message_contains(response, "Could not find 'user' with the name 'not-a-user'")
+  test "attaching a chat handle to a non-existent user fails" do
+    error = new_req(args: ["attach-handle", "not-a-user", "test-handle"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Could not find 'user' with the name 'not-a-user'")
   end
 
-  test "attaching a chat handle without specifying a chat handle fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user attach-handle dummy")
-    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 2")
+  test "attaching a chat handle without specifying a chat handle fails" do
+    error = new_req(args: ["attach-handle", "dummy"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Not enough args. Arguments required: exactly 2.")
   end
 
-  test "attaching a chat handle without specifying a user or chat handle fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user attach-handle")
-    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 2")
+  test "attaching a chat handle without specifying a user or chat handle fails" do
+    error = new_req(args: ["attach-handle"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Not enough args. Arguments required: exactly 2.")
   end
 
-  test "detaching a chat handle works", %{user: user} do
+  test "detaching a chat handle works" do
     user("dummy")
     |> with_chat_handle_for("test")
 
-    [payload] = user
-    |> send_message("@bot: operable:user detach-handle dummy")
+    payload = new_req(args: ["detach-handle", "dummy"])
+              |> send_req()
+              |> unwrap()
 
     assert %{chat_provider: %{name: "test"},
              username: "dummy"} = payload
   end
 
-  test "detaching a chat handle works even if there wasn't a handle to begin with", %{user: user} do
+  test "detaching a chat handle works even if there wasn't a handle to begin with" do
     user("dummy")
 
-    [payload] = user
-    |> send_message("@bot: operable:user detach-handle dummy")
+    payload = new_req(args: ["detach-handle", "dummy"])
+              |> send_req()
+              |> unwrap()
 
     assert %{chat_provider: %{name: "test"},
              username: "dummy"} = payload
   end
 
-  test "detaching a chat handle from a non-existent user fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user detach-handle not-a-user")
-    assert_error_message_contains(response, "Could not find 'user' with the name 'not-a-user'")
+  test "detaching a chat handle from a non-existent user fails" do
+    error = new_req(args: ["detach-handle", "not-a-user"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Could not find 'user' with the name 'not-a-user'")
   end
 
-  test "detaching a chat handle without specifying a user fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user detach-handle")
-    assert_error_message_contains(response, "Not enough args. Arguments required: exactly 1")
+  test "detaching a chat handle without specifying a user fails" do
+    error = new_req(args: ["detach-handle"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Not enough args. Arguments required: exactly 1.")
   end
 
-  test "listing chat handles works", %{user: user} do
-    user("dummy")
-    |> with_chat_handle_for("test")
+  test "listing chat handles works", %{users: users} do
+    Enum.each(users, &(with_chat_handle_for(&1, "test")))
 
-    payload = user
-    |> send_message("@bot: operable:user list-handles")
-    |> Enum.sort_by(fn(h) -> h[:username] end)
+    payload = new_req(args: ["list-handles"])
+              |> send_req()
+              |> unwrap()
+              |> Enum.sort_by(fn(h) -> h[:username] end)
 
-    assert [%{username: "dummy",
-              handle: "dummy"},
+    assert [%{username: "admin",
+              handle: "admin"},
             %{username: "tester",
               handle: "tester"}] = payload
   end
 
-  test "passing an unknown subcommand fails", %{user: user} do
-    response = send_message(user, "@bot: operable:user not-a-subcommand")
-    assert_error_message_contains(response, "Unknown subcommand 'not-a-subcommand'")
+  test "passing an unknown subcommand fails" do
+    error = new_req(args: ["not-a-subcommand"])
+            |> send_req()
+            |> unwrap_error()
+
+    assert(error == "Unknown subcommand 'not-a-subcommand'")
   end
 
+
+  #### Setup Functions ####
+
+  defp with_users(_) do
+    [users: [user("admin"), user("tester")]]
+  end
 end
