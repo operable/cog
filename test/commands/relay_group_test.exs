c790b5f4a201285d16dfc6fc98e8cc87e91f8a84
Matthew Peck <matt@operable.io>
2016-11-14 11:15:36 -0500

    relay group test updates

--- test/commands/relay_group_test.exs
+++ test/commands/relay_group_test.exs
@@ -1,149 +1,154 @@
 defmodule Cog.Test.Commands.RelayGroupTest do
-  use Cog.AdapterCase, adapter: "test"
+  use Cog.CommandCase, command_module: Cog.Commands.RelayGroup
 
-  @moduletag :skip
+  import Cog.Support.ModelUtilities, only: [relay_group: 1,
+                                            relay: 2,
+                                            add_relay_to_group: 2,
+                                            bundle_version: 1,
+                                            assign_bundle_to_group: 2]
+  alias Cog.Repository.RelayGroups
 
-  alias Cog.Support.ModelUtilities
-  alias Cog.Models.RelayGroup
-  alias Cog.Repo
+  test "listing relay groups work" do
+    relay_group("relay_group1")
+    relay_group("relay_group2")
 
-  setup do
-    user = user("vanstee", first_name: "Patrick", last_name: "Van Stee")
-    |> with_chat_handle_for("test")
-    |> with_permission("operable:manage_relays")
+    response = new_req(args: ["list"])
+               |> send_req()
+               |> unwrap()
 
-    {:ok, %{user: user}}
+    assert([%{name: "relay_group1"},
+            %{name: "relay_group2"}] = response)
   end
 
-  test "listing relay groups work", %{user: user} do
-    # Create a few relays
-    relay_group1 = ModelUtilities.relay_group("relay_group1")
-    relay_group2 = ModelUtilities.relay_group("relay_group2")
+  test "listing with no relay groups returns an empty list" do
+    payload = new_req(args: ["list"])
+              |> send_req()
+              |> unwrap()
 
-    # Check to see that they show up in the list
-    [foo1, foo2] = send_message(user, "@bot: operable:relay-group list")
-
-    assert foo1.name == "relay_group1"
-    assert foo1.id == relay_group1.id
-    assert foo2.name == "relay_group2"
-    assert foo2.id == relay_group2.id
+    assert([] == payload)
   end
 
-  test "listing with no relay groups returns an empty list", %{user: user} do
-    payload = send_message(user, "@bot: operable:relay-group list")
-    assert "Pipeline executed successfully, but no output was returned" = payload
+  test "listing is the default operation" do
+    payload = new_req()
+              |> send_req()
+              |> unwrap()
+
+    assert([] == payload)
   end
 
-  test "listing is the default operation", %{user: user} do
-    payload = send_message(user, "@bot: operable:relay-group")
-    assert "Pipeline executed successfully, but no output was returned" = payload
+  test "renaming a relay group" do
+    %{id: id} = relay_group("relay_group")
+
+    response = new_req(args: ["rename", "relay_group", "rly_grp"])
+               |> send_req()
+               |> unwrap()
+
+    assert(%{old_name: "relay_group",
+             relay_group: %{name: "rly_grp",
+                            id: ^id}} = response)
+
+    relay_group = RelayGroups.by_id!(id)
+    assert(relay_group.name == "rly_grp")
   end
 
-  test "renaming a relay group", %{user: user} do
-    relay_group = ModelUtilities.relay_group("relay_group")
+  test "creating a relay group" do
+    response = new_req(args: ["create", "foogroup"])
+               |> send_req()
+               |> unwrap()
 
-    [response] = send_message(user, "@bot: operable:relay-group rename relay_group rly_grp")
+    assert(%{name: "foogroup"} = response)
 
-    assert response.relay_group.name == "rly_grp"
-
-    relay_group = Repo.get(RelayGroup, relay_group.id)
-    assert relay_group.name == "rly_grp"
+    relay_group = RelayGroups.by_id!(response.id)
+    assert(relay_group.name == "foogroup")
   end
 
-  test "creating a relay group", %{user: user} do
-    [response] = send_message(user, "@bot: operable:relay-group create foogroup")
-    assert response.name == "foogroup"
+  test "deleting a relay group" do
+    relay_group("relay_group")
 
-    relay_group = Repo.get(RelayGroup, response.id)
-    assert relay_group.name == "foogroup"
+    response = new_req(args: ["delete", "relay_group"])
+               |> send_req()
+               |> unwrap()
+
+    assert(%{name: "relay_group"} = response)
+
+    relay_group = RelayGroups.by_id(response.id)
+                  |> unwrap_error()
+
+    assert(relay_group == :not_found)
   end
 
-  test "deleting a relay group", %{user: user} do
-    ModelUtilities.relay_group("relay_group")
+  test "adding relays to relay groups" do
+    relay_group("relay_group")
+    relay("relay", "foo")
 
-    [response] = send_message(user, "@bot: operable:relay-group delete relay_group")
+    response = new_req(args: ["member", "add", "relay_group", "relay"])
+               |> send_req()
+               |> unwrap()
 
-    assert response.name == "relay_group"
+    assert(%{name: "relay_group",
+             relays: [%{name: "relay"}]} = response)
 
-    fetched_relay_group = Repo.get(RelayGroup, response.id)
-    assert fetched_relay_group == nil
+    relay_group = RelayGroups.by_id!(response.id)
+    assert(%{name: "relay_group",
+             relays: [%{name: "relay"}]} = relay_group)
   end
 
-  test "adding relays to relay groups", %{user: user} do
-    ModelUtilities.relay_group("relay_group")
-    relay = ModelUtilities.relay("relay", "foo")
+  test "removing relays from relay groups" do
+    relay_group = relay_group("relay_group")
+    relay = relay("relay", "foo")
+    add_relay_to_group(relay_group.id, relay.id)
 
-    [response] = send_message(user, "@bot: operable:relay-group member add relay_group relay")
+    response = new_req(args: ["member", "remove", "relay_group", "relay"])
+               |> send_req()
+               |> unwrap()
 
-    assert response.name == "relay_group"
-    assert length(response.relays) == 1
+    assert(%{name: "relay_group",
+             relays: []} = response)
 
-    decoded_relay = hd(response.relays)
-    assert decoded_relay.id == relay.id
-
-    fetched_relay_group = Repo.get(RelayGroup, response.id)
-    |> Repo.preload([:relays])
-    assert fetched_relay_group.name == "relay_group"
-
-    fetched_relay = hd(fetched_relay_group.relays)
-    assert fetched_relay.id == relay.id
+    relay_group = RelayGroups.by_id!(response.id)
+    assert(%{name: "relay_group",
+             relays: []} = relay_group)
   end
 
-  test "removing relays from relay groups", %{user: user} do
-    relay_group = ModelUtilities.relay_group("relay_group")
-    relay = ModelUtilities.relay("relay", "foo")
-    ModelUtilities.add_relay_to_group(relay_group.id, relay.id)
+  test "assigning bundles to relay groups" do
+    relay_group("relay_group")
+    bundle_version("bundle").bundle
 
-    [response] = send_message(user, "@bot: operable:relay-group member remove relay_group relay")
+    response = new_req(args: ["member", "assign", "relay_group", "bundle"])
+               |> send_req()
+               |> unwrap()
 
-    assert response.name == "relay_group"
-    assert length(response.relays) == 0
+    assert(%{name: "relay_group",
+             bundles: [%{name: "bundle"}]} = response)
 
-    fetched_relay_group = Repo.get(RelayGroup, response.id)
-    |> Repo.preload([:relays])
-    assert fetched_relay_group.name == "relay_group"
-    assert length(fetched_relay_group.relays) == 0
+    relay_group = RelayGroups.by_id!(response.id)
+    assert(%{name: "relay_group",
+             bundles: [%{name: "bundle"}]} = relay_group)
   end
 
-  test "assigning bundles to relay groups", %{user: user} do
-    ModelUtilities.relay_group("relay_group")
-    bundle = ModelUtilities.bundle_version("bundle").bundle
+  test "unassigning bundles from relay groups" do
+    relay_group = relay_group("relay_group")
+    bundle = bundle_version("bundle").bundle
+    assign_bundle_to_group(relay_group.id, bundle.id)
 
-    [response] = send_message(user, "@bot: operable:relay-group member assign relay_group bundle")
+    response = new_req(args: ["member", "unassign", "relay_group", "bundle"])
+               |> send_req()
+               |> unwrap()
 
-    assert response.name == "relay_group"
-    assert length(response.bundles) == 1
+    assert(%{name: "relay_group",
+             bundles: []} = response)
 
-    decoded_bundle = hd(response.bundles)
-    assert decoded_bundle.name == "bundle"
-
-    fetched_relay_group = Repo.get(RelayGroup, response.id)
-    |> Repo.preload([:bundles])
-    assert fetched_relay_group.name == "relay_group"
-
-    fetched_bundle = hd(fetched_relay_group.bundles)
-    assert fetched_bundle.id == bundle.id
+    relay_group = RelayGroups.by_id!(response.id)
+    assert(%{name: "relay_group",
+             bundles: []} = relay_group)
   end
 
-  test "unassigning bundles from relay groups", %{user: user} do
-    relay_group = ModelUtilities.relay_group("relay_group")
-    bundle = ModelUtilities.bundle_version("bundle").bundle
-    ModelUtilities.assign_bundle_to_group(relay_group.id, bundle.id)
+  test "passing an unknown subcommand fails" do
+    error = new_req(args: ["not-a-subcommand"])
+            |> send_req()
+            |> unwrap_error()
 
-    [response] = send_message(user, "@bot: operable:relay-group member unassign relay_group bundle")
-
-    assert response.name == "relay_group"
-    assert length(response.bundles) == 0
-
-    fetched_relay_group = Repo.get(RelayGroup, response.id)
-    |> Repo.preload([:bundles])
-    assert fetched_relay_group.name == "relay_group"
-    assert length(fetched_relay_group.bundles) == 0
-  end
-
-  test "passing an unknown subcommand fails", %{user: user} do
-    response = send_message(user, "@bot: operable:relay-group not-a-subcommand")
-    assert_error_message_contains(response, "Unknown subcommand 'not-a-subcommand'")
+    assert(error == "Unknown subcommand 'not-a-subcommand'")
   end
 
 end
