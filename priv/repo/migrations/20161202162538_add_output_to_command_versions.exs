2901eff70cb00d077d85ab350eaf420a4d589821
Patrick Van Stee <patrick@vanstee.me>
2016-12-06 11:23:51 -0500

    Revert "Command output docs"

--- priv/repo/migrations/20161202162538_add_output_to_command_versions.exs
+++
@@ -1,9 +0,0 @@
-defmodule Cog.Repo.Migrations.AddOutputToCommandVersions do
-  use Ecto.Migration
-
-  def change do
-    alter table(:command_versions) do
-      add :output, :string
-    end
-  end
-end
