a81d0a4d1bf5e62edbb45ecf462d6f9d3c3d4002
Matthew Peck <matt@operable.io>
2016-12-12 16:36:24 -0500

    trigger-create

--- lib/cog/commands/trigger/create.ex
+++ lib/cog/commands/trigger/create.ex
@@ -1,53 +1,43 @@
 defmodule Cog.Commands.Trigger.Create do
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "trigger-create"
+
   alias Cog.Repository.Triggers
   require Cog.Commands.Helpers, as: Helpers
 
-  Helpers.usage """
-  Create a pipeline trigger.
+  @description "Create a pipeline trigger."
 
-  USAGE
-    trigger create [FLAGS] <name> <pipeline> [OPTIONS]
+  @arguments "<name> <pipeline>"
 
-  ARGS
-    name       The human-readable unique name for a trigger
-    pipeline   Entire command pipeline text.
-
-  FLAGS
-    -h, --help  Display this usage info
-
-  OPTIONS
-    -e, --enabled     (Boolean) Should the trigger be created in an enabled
-                      state, or not? Defaults to true.
-    -d, --description (String) Free text description of the trigger. Defaults to nil.
-    -u, --as-user     (String) The Cog username the trigger will execute as. Defaults to nil.
-    -t, --timeout-sec (Integer) Amount of time Cog will wait for
-                      execution to finish. Defaults to 30. Must be greater than 0
-
-  For more detail on these, consult http://docs.operable.io/docs/triggers
-
-  EXAMPLES
-
-    trigger create my-trigger "echo 'Hello World'" -d "A friendly greeting"
-
+  @examples """
+  trigger create my-trigger "echo 'Hello World'" -d "A friendly greeting"
   """
 
-  def create(%{options: %{"help" => true}}, _args),
-    do: show_usage
-  def create(req, [name, pipeline]),
-    do: do_create(name, pipeline, req.options)
-  def create(_req, _args),
-    do: {:error, :invalid_args}
+  option "enabled", type: "bool", short: "e", description: "Should the trigger be created in an enabled state, or not? Defaults to true."
+  option "description", type: "string", short: "d", description: "Free text description of the trigger. Defaults to nil."
+  option "timeout-sec", type: "int", short: "t", description: "Amount of time Cog will wait for execution to finish. Defaults to 30. Must be greater than 0"
+  option "as-user", type: "string", short: "u", description: "The Cog username the trigger will execute as. Defaults to nil."
 
-  defp do_create(name, pipeline, options) do
-    params = Cog.Command.Trigger.Helpers.normalize_params(options)
-    |> Map.put("pipeline", pipeline)
-    |> Map.put("name", name)
+  def handle_message(req, state) do
+    results = with {:ok, [name, pipeline]} <- Helpers.get_args(req.args, 2) do
+      params = Cog.Command.Trigger.Helpers.normalize_params(req.options)
+      |> Map.put("pipeline", pipeline)
+      |> Map.put("name", name)
 
-    case Triggers.new(params) do
-      {:ok, trigger} ->
-        {:ok, "trigger-create", trigger}
+      case Triggers.new(params) do
+        {:ok, trigger} ->
+          {:ok, trigger}
+        {:error, error} ->
+          {:error, {:trigger_invalid, error}}
+      end
+    end
+
+    case results do
+      {:ok, data} ->
+        {:reply, req.reply_to, "trigger-create", data, state}
       {:error, error} ->
-        {:error, {:trigger_invalid, error}}
+        {:error, req.reply_to, Helpers.error(error), state}
     end
   end
 end
