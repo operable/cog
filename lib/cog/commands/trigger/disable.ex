38b2822a1391987044bd6f8dfa6bf4eace7aed3a
Matthew Peck <matt@operable.io>
2016-12-12 17:20:52 -0500

    trigger-disable

--- lib/cog/commands/trigger/disable.ex
+++ lib/cog/commands/trigger/disable.ex
@@ -1,33 +1,37 @@
 defmodule Cog.Commands.Trigger.Disable do
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "trigger-disable"
+
   require Cog.Commands.Helpers, as: Helpers
 
-  Helpers.usage """
-  Disable a pipeline trigger. Provided as a convenient alternative to `trigger update <trigger-name> --enabled=false`.
+  @description "Disable a pipeline trigger. Provided as a convenient alternative to `trigger update <trigger-name> --enabled=false`."
 
-  USAGE
-    trigger disable [FLAGS] <name>
+  @arguments "<name>"
 
-  ARGS
-    name  Specifies the trigger to disable.
-
-  FLAGS
-    -h, --help  Display this usage info
-
-  EXAMPLES
-
-    trigger disable my-trigger
+  @examples """
+  trigger disable my-trigger
   """
 
-  def disable(%{options: %{"help" => true}}, _args),
-    do: show_usage
-  def disable(req, [name]) do
-    options = %{"enabled" => false}
-    req = %{req | options: options}
-    case Cog.Commands.Trigger.Update.update(req, [name]) do
-      {:ok, _, data} ->
-        {:ok, "trigger-disable", data}
-      other ->
-        other
+  rule "when command is #{Cog.Util.Misc.embedded_bundle}:trigger-disable must have #{Cog.Util.Misc.embedded_bundle}:manage_triggers"
+
+  def handle_message(req, state) do
+    result = with {:ok, [name]} <- Helpers.get_args(req.args, 1) do
+      options = %{"enabled" => false}
+      req = %{req | options: options}
+      case Cog.Commands.Trigger.Update.update(req, [name]) do
+        {:ok, _, data} ->
+          {:ok, data}
+        other ->
+          other
+      end
+    end
+
+    case result do
+      {:ok, data} ->
+        {:reply, req.reply_to, "trigger-disable", data, state}
+      {:error, error} ->
+        {:error, req.reply_to, Helpers.error(error), state}
     end
   end
 
