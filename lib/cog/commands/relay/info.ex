da21a7867f74d88fb5e278b52694d57663737743
vanstee <patrick@vanstee.me>
2016-12-13 09:27:53 -0500

    Move relay-info to its own command

--- lib/cog/commands/relay/info.ex
+++ lib/cog/commands/relay/info.ex
@@ -1,41 +1,62 @@
 defmodule Cog.Commands.Relay.Info do
-  require Cog.Commands.Helpers, as: Helpers
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "relay-info"
 
-  alias Cog.Repository.Relays
+  alias Cog.Commands.Relay
   alias Cog.Commands.Relay.ViewHelpers
+  alias Cog.Repository.Relays
 
-  Helpers.usage """
-  Get detailed information about a relay.
+  @description "Get detailed information about a relay"
 
-  USAGE
-    relay info [FLAGS] <name>
+  @arguments "<name>"
 
-  ARGS
-    name    The relay to get info about
-
-  FLAGS
-    -h, --help    Display this usage info
-
-  EXAMPLES
+  @examples """
+  View info for relay:
 
     relay info foo
   """
 
-  def info(%{options: %{"help" => true}}, _args),
-    do: show_usage
-  def info(_req, [name]) when is_binary(name) do
-    case Relays.by_name(name) do
+  @output_description "Returns the serialized relay including relay groups"
+
+  @output_example """
+  [
+    {
+      "status": "enabled",
+      "relay_groups": [],
+      "name": "default",
+      "id": "9e173ffd-b247-4833-80d4-a87c4175732d",
+      "created_at": "2016-12-13T14:33:48",
+      "_show_groups": true
+    }
+  ]
+  """
+
+  rule "when command is #{Cog.Util.Misc.embedded_bundle}:relay-info must have #{Cog.Util.Misc.embedded_bundle}:manage_relays"
+
+  option "group", type: "bool", short: "g"
+  option "verbose", type: "bool", short: "v"
+
+  def handle_message(req = %{args: [name]}, state) when is_binary(name) do
+    result = case Relays.by_name(name) do
       {:ok, relay} ->
         {:ok, "relay-info", ViewHelpers.render(relay, %{"group" => true})}
       {:error, :not_found} ->
         {:error, {:resource_not_found, "relay", name}}
     end
+
+    case result do
+      {:ok, template, data} ->
+        {:reply, req.reply_to, template, data, state}
+      {:error, err} ->
+        {:error, req.reply_to, Relay.error(err), state}
+    end
   end
-  def info(_, [_]),
-    do: {:error, :wrong_type}
-  def info(_, []),
-    do: {:error, {:not_enough_args, 1}}
-  def info(_, _),
-    do: {:error, {:too_many_args, 1}}
+  def handle_message(req = %{args: [_invalid_permission]}, state),
+    do: {:error, req.reply_to, Relay.error(:wrong_type), state}
+  def handle_message(req = %{args: []}, state),
+    do: {:error, req.reply_to, Relay.error({:not_enough_args, 1}), state}
+  def handle_message(req, state),
+    do: {:error, req.reply_to, Relay.error({:too_many_args, 1}), state}
 
 end
