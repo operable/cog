2c69c6d52073cfdd3697f6997c9c1679c4b52169
Matthew Peck <matt@operable.io>
2016-12-13 17:03:30 -0500

    add output examples

--- lib/cog/commands/trigger/list.ex
+++ lib/cog/commands/trigger/list.ex
@@ -7,6 +7,23 @@ defmodule Cog.Commands.Trigger.List do
 
   @description "List all triggers."
 
+  @output_description "Returns a list of triggers."
+
+  @output_example """
+  [
+    {
+      "timeout_sec": 30,
+      "pipeline": "echo fobar",
+      "name": "foo",
+      "invocation_url": "http://localhost:4001/v1/triggers/00000000-0000-0000-0000-000000000000",
+      "id": "00000000-0000-0000-0000-000000000000",
+      "enabled": false,
+      "description": null,
+      "as_user": null
+    }
+  ]
+  """
+
   permission "manage_triggers"
 
   rule "when command is #{Cog.Util.Misc.embedded_bundle}:trigger-list must have #{Cog.Util.Misc.embedded_bundle}:manage_triggers"
