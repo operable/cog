9c2c4c7daeeb88fbbe3d35622a6fc45b5e61be98
vanstee <patrick@vanstee.me>
2016-12-13 01:45:15 -0500

    Move bundle-disable to its own command

--- test/commands/bundle_test.exs
+++ test/commands/bundle_test.exs
@@ -1,7 +1,7 @@
 defmodule Cog.Test.Commands.BundleTest do
   use Cog.CommandCase, command_module: Cog.Commands.Bundle
 
-  alias Cog.Commands.Bundle.{List, Versions, Info, Enable}
+  alias Cog.Commands.Bundle.{List, Versions, Info, Enable, Disable}
   alias Cog.Repository.Bundles
   import Cog.Support.ModelUtilities, only: [bundle_version: 1,
                                             bundle_version: 2]
@@ -53,8 +53,8 @@ defmodule Cog.Test.Commands.BundleTest do
     bundle_version = bundle_version("test_bundle")
     Bundles.set_bundle_version_status(bundle_version, :enabled)
 
-    {:ok, response} = new_req(args: ["disable", "test_bundle"])
-    |> send_req()
+    {:ok, response} = new_req(args: ["test_bundle"])
+    |> send_req(Disable)
 
     assert(response == %{name: "test_bundle", status: "disabled", version: "0.1.0"})
     refute(Bundles.enabled?(bundle_version))
