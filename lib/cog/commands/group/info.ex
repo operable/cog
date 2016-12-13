84b491fc80d1d7ecf115400648193c77d2817d42
vanstee <patrick@vanstee.me>
2016-12-13 07:51:36 -0500

    Move group-info to its own command

--- lib/cog/commands/group/info.ex
+++ lib/cog/commands/group/info.ex
@@ -1,38 +1,62 @@
 defmodule Cog.Commands.Group.Info do
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "group-info"
+
   require Cog.Commands.Helpers, as: Helpers
+  alias Cog.Commands.Group
   alias Cog.Repository.Groups
 
-  Helpers.usage """
-  Get info about a user group.
+  @description "Get info about a user group"
 
-  USAGE
-    group info <group-name>
+  @arguments "<group-name>"
 
-  ARGS
-    group-name    The user group name to get info about
+  @output_description "Returns the serialized group"
 
-  FLAGS
-    -h, --help    Display this usage info
+  @output_example """
+  [
+    {
+      "roles": [
+        {
+          "name": "deploy",
+          "id": "85c8d788-b368-472c-a83c-fd175d8c4c35"
+        }
+      ],
+      "name": "engineering",
+      "members": [
+        {
+          "username": "vanstee",
+          "last_name": "Van Stee",
+          "id": "7cb7fba2-ea65-46a0-a8af-bea71df1ac00",
+          "first_name": "Patrick",
+          "email_address": "patrick@operable.io"
+        }
+      ],
+      "id": "0640f71f-3c1b-4d47-8b8b-c8765b115872"
+    }
+  ]
   """
 
-  @spec get_info(%Cog.Messages.Command{}, List.t) :: {:ok, String.t, Map.t} | {:error, any()}
-  def get_info(req, arg_list) do
-    if Helpers.flag?(req.options, "help") do
-      show_usage
-    else
-      case Helpers.get_args(arg_list, 1) do
-        {:ok, [group_name]} ->
-          case Groups.by_name(group_name) do
-            {:ok, group} ->
-              {:ok, "user-group-info", group}
-            {:error, :not_found} ->
-              {:error, {:resource_not_found, "user group", group_name}}
-          end
-        {:error, {:not_enough_args, _}} ->
-          show_usage("Missing required argument: group_name")
-        {:error, {:too_many_args, _}} ->
-          show_usage("Too many arguments. You can only get info for one user group at a time.")
-      end
+  rule ~s(when command is #{Cog.Util.Misc.embedded_bundle}:group-info must have #{Cog.Util.Misc.embedded_bundle}:manage_groups)
+
+  def handle_message(req, state) do
+    result = case Helpers.get_args(req.args, 1) do
+      {:ok, [group_name]} ->
+        case Groups.by_name(group_name) do
+          {:ok, group} ->
+            {:ok, "user-group-info", group}
+          {:error, :not_found} ->
+            {:error, {:resource_not_found, "user group", group_name}}
+        end
+      error ->
+        error
+    end
+
+    case result do
+      {:ok, template, data} ->
+        {:reply, req.reply_to, template, data, state}
+      {:error, err} ->
+        {:error, req.reply_to, Group.error(err), state}
     end
   end
 end
