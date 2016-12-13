09d406f345a708391bb3215097e9be28a5111c97
vanstee <patrick@vanstee.me>
2016-12-13 17:21:43 -0500

    Move relay-group-list to its own command

--- lib/cog/commands/relay_group/list.ex
+++ lib/cog/commands/relay_group/list.ex
@@ -1,27 +1,42 @@
 defmodule Cog.Commands.RelayGroup.List do
-  require Cog.Commands.Helpers, as: Helpers
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "relay-group-list"
+
   alias Cog.Repository.RelayGroups
   alias Cog.Commands.RelayGroup
 
-  Helpers.usage """
-  List all relay groups
+  @description "List all relay groups"
 
-  USAGE
-    relay-group list [FLAGS]
+  @output_description "Lists serialized relay groups including their relays"
 
-  FLAGS
-    -h, --help      Display this usage list
+  @output_example """
+  [
+    {
+      "relays": [
+        {
+          "status": "enabled",
+          "name": "production",
+          "id": "9e173ffd-b247-4833-80d4-a87c4175732d",
+          "created_at": "2016-12-13T14:33:48"
+        }
+      ],
+      "name": "default",
+      "id": "ee3d7b91-9c66-487d-b250-8df47e7f7a32",
+      "created_at": "2016-12-14T00:11:14",
+      "bundles": []
+    }
+  ]
   """
 
-  def list(req, _args) do
-    if Helpers.flag?(req.options, "help") do
-      show_usage
-    else
-      {:ok, "relay-group-list", generate_response(RelayGroups.all)}
-    end
+  permission "manage_relays"
+
+  rule "when command is #{Cog.Util.Misc.embedded_bundle}:relay-group-list must have #{Cog.Util.Misc.embedded_bundle}:manage_relays"
+
+  def handle_message(req, state) do
+    relay_groups = RelayGroups.all
+    |> Enum.map(&RelayGroup.json/1)
+
+    {:reply, req.reply_to, "relay-group-list", relay_groups, state}
   end
-
-  defp generate_response(relay_groups),
-    do: Enum.map(relay_groups, &RelayGroup.json/1)
-
 end
