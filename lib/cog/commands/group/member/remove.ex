35e6f20ca27d6832067b26e1cd23595998726333
vanstee <patrick@vanstee.me>
2016-12-13 08:04:11 -0500

    Move group-member-remove to its own command

--- lib/cog/commands/group/member/remove.ex
+++ lib/cog/commands/group/member/remove.ex
@@ -1,38 +1,50 @@
 defmodule Cog.Commands.Group.Member.Remove do
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "group-member-remove"
+
   require Cog.Commands.Helpers, as: Helpers
+  alias Cog.Commands.Group
   alias Cog.Repository.Groups
   alias Cog.Repository.Users
 
-  Helpers.usage """
-  Removes users from user groups.
+  @description "Removes users from user groups"
 
-  USAGE
-    group member remove [FLAGS] <group-name> <user-name ...>
+  @arguments "<group-name> <user-name ...>"
 
-  ARGS
-    group-name    The group to add users to
-    user-name     List of one or more users to add to the group
+  @output_description "Returns the serialized group without the removed member"
 
-  FLAGS
-    -h, --help    Display this usage info
+  @output_example """
+  [
+    {
+      "roles": [],
+      "name": "engineering",
+      "members": [],
+      "id": "0640f71f-3c1b-4d47-8b8b-c8765b115872"
+    }
+  ]
   """
 
-  @spec remove_user(%Cog.Messages.Command{}, List.t) :: {:ok, String.t, Map.t} | {:error, any()}
-  def remove_user(req, arg_list) do
-    if Helpers.flag?(req.options, "help") do
-      show_usage
-    else
-      case Helpers.get_args(arg_list, min: 2) do
-        {:ok, [group_name | usernames]} ->
-          case remove(group_name, usernames) do
-            {:ok, group} ->
-              {:ok, "user-group-update-success", group}
-            error ->
-              error
-          end
-        {:error, {:under_min_args, _min}} ->
-          show_usage(error(:missing_args))
-      end
+  rule ~s(when command is #{Cog.Util.Misc.embedded_bundle}:group-member-remove must have #{Cog.Util.Misc.embedded_bundle}:manage_users)
+
+  def handle_message(req, state) do
+    result = case Helpers.get_args(req.args, min: 2) do
+      {:ok, [group_name | usernames]} ->
+        case remove(group_name, usernames) do
+          {:ok, group} ->
+            {:ok, "user-group-update-success", group}
+          error ->
+            error
+        end
+      error -> 
+        error
+    end
+
+    case result do
+      {:ok, template, data} ->
+        {:reply, req.reply_to, template, data, state}
+      {:error, err} ->
+        {:error, req.reply_to, Group.error(err), state}
     end
   end
 
@@ -50,9 +62,5 @@ defmodule Cog.Commands.Group.Member.Remove do
       {:error, :not_found} ->
         {:error, {:resource_not_found, "user group", group_name}}
     end
-  end
-
-  defp error(:missing_args) do
-    "Missing required args. At a minimum you must include the user group and at least one user name to remove"
   end
 end
