6e489bc39dfe727e14991ed2af51f3854e525312
vanstee <patrick@vanstee.me>
2016-12-13 01:37:57 -0500

    Move bundle-enable to its own command

--- test/commands/bundle_test.exs
+++ test/commands/bundle_test.exs
@@ -1,7 +1,7 @@
 defmodule Cog.Test.Commands.BundleTest do
   use Cog.CommandCase, command_module: Cog.Commands.Bundle
 
-  alias Cog.Commands.Bundle.{List, Versions, Info}
+  alias Cog.Commands.Bundle.{List, Versions, Info, Enable}
   alias Cog.Repository.Bundles
   import Cog.Support.ModelUtilities, only: [bundle_version: 1,
                                             bundle_version: 2]
@@ -42,8 +42,8 @@ defmodule Cog.Test.Commands.BundleTest do
   test "enable a bundle" do
     bundle_version = bundle_version("test_bundle")
 
-    {:ok, response} = new_req(args: ["enable", "test_bundle"])
-    |> send_req()
+    {:ok, response} = new_req(args: ["test_bundle"])
+    |> send_req(Enable)
 
     assert(response == %{name: "test_bundle", status: "enabled", version: "0.1.0"})
     assert(Bundles.enabled?(bundle_version))
