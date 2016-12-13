166ffbd92621b4c59f93497946eff17ba817fde3
Matthew Peck <matt@operable.io>
2016-12-13 15:04:57 -0500

    add output examples

--- lib/cog/commands/user/info.ex
+++ lib/cog/commands/user/info.ex
@@ -1,35 +1,79 @@
 defmodule Cog.Commands.User.Info do
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "user-info"
+
   alias Cog.Repository.Users
   require Cog.Commands.Helpers, as: Helpers
 
-  Helpers.usage """
-  Show detailed information about a specific user.
+  @description "Show detailed information about a specific user."
 
-  USAGE
-    user info [FLAGS] <username>
+  @arguments "<username>"
 
-  ARGS
-    username   The name of a user
+  @output_description "Returns a single user; their groups, roles, permissions and chat handles."
 
-  FLAGS
-    -h, --help  Display this usage info
+  @output_example """
+  [
+    {
+      "username": "Cog",
+      "last_name": "McCog",
+      "id": "00000000-0000-0000-0000-000000000000",
+      "groups": [
+        {
+          "roles": [
+            {
+              "permissions": [
+                {
+                  "name": "manage_users",
+                  "id": "00000000-0000-0000-0000-000000000000",
+                  "bundle": "operable"
+                },
+              ],
+              "name": "cog-admin",
+              "id": "00000000-0000-0000-0000-000000000000"
+            }
+          ],
+          "name": "cog-admin",
+          "id": "00000000-0000-0000-0000-000000000000"
+        }
+      ],
+      "first_name": "Cog",
+      "email_address": "cog@localhost",
+      "chat_handles": [
+        {
+          "username": "admin",
+          "id": "00000000-0000-0000-0000-000000000000",
+          "handle": "cog",
+          "chat_provider": {
+            "name": "slack"
+          }
+        }
+      ]
+    }
+  ]
   """
 
-  def info(%{options: %{"help" => true}}, _args) do
-    show_usage
-  end
-  def info(_req, [user_name]) do
-    case Users.by_username(user_name) do
-      {:error, :not_found} ->
-        {:error, {:resource_not_found, "user", user_name}}
-      {:ok, user} ->
-        rendered = Cog.V1.UserView.render("show.json", %{user: user})
-        {:ok, "user-info", rendered[:user]}
+  permission "manage_users"
+
+  rule "when command is #{Cog.Util.Misc.embedded_bundle}:user-info must have #{Cog.Util.Misc.embedded_bundle}:manage_users"
+
+  def handle_message(req, state) do
+    results = with {:ok, [user_name]} <- Helpers.get_args(req.args, 1) do
+      case Users.by_username(user_name) do
+        {:error, :not_found} ->
+          {:error, {:resource_not_found, "user", user_name}}
+        {:ok, user} ->
+          rendered = Cog.V1.UserView.render("show.json", %{user: user})
+          {:ok, rendered[:user]}
+      end
+    end
+
+    case results do
+      {:ok, data} ->
+        {:reply, req.reply_to, "user-info", data, state}
+      {:error, error} ->
+        {:error, req.reply_to, Helpers.error(error), state}
     end
   end
-  def info(_req, []),
-    do: {:error, {:not_enough_args, 1}}
-  def info(_req, _),
-    do: {:error, {:too_many_args, 1}}
 
 end
