8e8ae2fa1135e4aab6caf3c3db0dedd3982c45c1
vanstee <patrick@vanstee.me>
2016-12-13 15:30:52 -0500

    Break down tests to match new commands

--- test/commands/alias/move_test.exs
+++ test/commands/alias/move_test.exs
@@ -1,7 +1,7 @@
-defmodule Cog.Test.Commands.AliasTest do
+defmodule Cog.Test.Commands.Alias.MoveTest do
   use Cog.CommandCase, command_module: Cog.Commands.Alias
 
-  alias Cog.Commands.Alias.{Create, Move, Delete, List}
+  alias Cog.Commands.Alias.Move
   import Cog.Support.ModelUtilities, only: [site_alias: 2,
                                             with_alias: 3,
                                             get_alias: 1,
@@ -9,56 +9,6 @@ defmodule Cog.Test.Commands.AliasTest do
                                             user: 1]
 
   setup :with_user
-
-  describe "alias creation" do
-    test "with standard args", %{user: user} do
-      {:ok, response} = new_req(user: %{"id" => user.id}, args: ["my-new-alias", "echo My New Alias"])
-      |> send_req(Create)
-
-      assert(%{name: "my-new-alias",
-               pipeline: "echo My New Alias",
-               visibility: "user"} = response)
-
-      created_alias = get_alias("my-new-alias", user.id)
-
-      assert(%{name: "my-new-alias",
-               pipeline: "echo My New Alias",
-               visibility: "user"} = created_alias)
-    end
-
-    test "with an existing name", %{user: user}=context do
-      with_user_alias(context)
-
-      {:error, error} = new_req(user: %{"id" => user.id}, args: ["my-new-alias", "echo My New Alias"])
-      |> send_req(Create)
-
-      assert(error == "name: The alias name is already in use.")
-    end
-  end
-
-  describe "alias removal" do
-    setup :with_user_alias
-
-    test "removing an alias", %{user: user} do
-      {:ok, response} = new_req(user: %{"id" => user.id}, args: ["my-new-alias"])
-      |> send_req(Delete)
-
-      assert(%{name: "my-new-alias",
-               pipeline: "echo My New Alias",
-               visibility: "user"} = response)
-
-      deleted_alias = get_alias("my-new-alias", user.id)
-
-      refute deleted_alias
-    end
-
-    test "removing an alias that does not exist", %{user: user} do
-      {:error, error} = new_req(user: %{"id" => user.id}, args: ["my-non-existant-alias"])
-      |> send_req(Delete)
-
-      assert(error == "I can't find 'my-non-existant-alias'. Please try again")
-    end
-  end
 
   describe "moving an alias to site" do
     setup :with_user_alias
@@ -264,101 +214,6 @@ defmodule Cog.Test.Commands.AliasTest do
       assert(get_alias("my-renamed-alias"))
     end
   end
-
-  describe "listing aliases" do
-    test "all", %{user: user} do
-      Enum.each(1..3, &with_alias(user, "my-new-alias#{&1}", "echo My New Alias"))
-      with_site_alias()
-
-      {:ok, response} = new_req(user: %{"id" => user.id}, args: [])
-      |> send_req(List)
-
-      assert([%{visibility: "site",
-                pipeline: "echo My New Alias",
-                name: "my-new-alias"},
-              %{visibility: "user",
-                pipeline: "echo My New Alias",
-                name: "my-new-alias1"},
-              %{visibility: "user",
-                pipeline: "echo My New Alias",
-                name: "my-new-alias2"},
-              %{visibility: "user",
-                pipeline: "echo My New Alias",
-                name: "my-new-alias3"}] == response)
-    end
-
-    test "matching a pattern", %{user: user} do
-      Enum.each(1..2, &with_alias(user, "my-new-alias#{&1}", "echo My New Alias"))
-      Enum.each(1..2, &with_alias(user, "new-alias#{&1}", "echo My New Alias"))
-      with_site_alias()
-
-      {:ok, response} = new_req(user: %{"id" => user.id}, args: ["my-*"])
-      |> send_req(List)
-
-      assert([%{visibility: "site",
-                pipeline: "echo My New Alias",
-                name: "my-new-alias"},
-              %{visibility: "user",
-                pipeline: "echo My New Alias",
-                name: "my-new-alias1"},
-              %{visibility: "user",
-                pipeline: "echo My New Alias",
-                name: "my-new-alias2"}] == response)
-    end
-
-    test "with no matching pattern", %{user: user} do
-      with_site_alias()
-      {:ok, response} = new_req(user: %{"id" => user.id}, args: ["their-*"])
-      |> send_req(List)
-
-      assert([] == response)
-    end
-
-    test "with no aliases", %{user: user} do
-      {:ok, response} = new_req(user: %{"id" => user.id})
-      |> send_req(List)
-
-      assert([] == response)
-    end
-
-    test "with an invalid pattern", %{user: user} do
-      {:error, error} = new_req(user: %{"id" => user.id}, args: ["% &my#-*"])
-      |> send_req(List)
-
-      assert(error == "Invalid alias name. Only emoji, letters, numbers, and the following special characters are allowed: *, -, _")
-    end
-
-    test "with too many wildcards", %{user: user} do
-      {:error, error} = new_req(user: %{"id" => user.id}, args: ["*my-*"])
-      |> send_req(List)
-
-      assert(error == "Too many wildcards. You can only include one wildcard in a query")
-    end
-
-    test "with a bad pattern and too many wildcards", %{user: user} do
-      {:error, error} = new_req(user: %{"id" => user.id}, args: ["*m++%y-*"])
-      |> send_req(List)
-
-      assert(error == "Too many wildcards. You can only include one wildcard in a query\nInvalid alias name. Only emoji, letters, numbers, and the following special characters are allowed: *, -, _")
-    end
-  end
-
-  describe "alias args" do
-    test "passing too many", %{user: user} do
-      {:error, error} = new_req(user: %{"id" => user.id}, args: ["my-invalid-alias", "echo foo", "invalid-arg"])
-      |> send_req(Create)
-
-      assert(error == "Too many args. Arguments required: exactly 2.")
-    end
-
-    test "passing too few", %{user: user} do
-      {:error, error} = new_req(user: %{"id" => user.id}, args: ["my-invalid-alias"])
-      |> send_req(Create)
-
-      assert(error == "Not enough args. Arguments required: exactly 2.")
-    end
-  end
-
 
   ### Context Functions ###
 
