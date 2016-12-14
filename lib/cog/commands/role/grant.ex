2e317b5c63708e33356b95e119dc7d0c186b76c7
vanstee <patrick@vanstee.me>
2016-12-14 16:08:26 -0500

    Move role-grant to its own command

--- lib/cog/commands/role/grant.ex
+++ lib/cog/commands/role/grant.ex
@@ -1,34 +1,53 @@
 defmodule Cog.Commands.Role.Grant do
-  alias Cog.Repository.Roles
-  alias Cog.Repository.Groups
+  use Cog.Command.GenCommand.Base,
+    bundle: Cog.Util.Misc.embedded_bundle,
+    name: "role-grant"
 
+  alias Cog.Commands
   alias Cog.Models.Role
+  alias Cog.Repository.{Groups, Roles}
 
-  require Cog.Commands.Helpers, as: Helpers
+  @description "Grant a role to a group"
 
-  Helpers.usage """
-  Grant a role to a group.
+  @arguments "<role> <group>"
 
-  USAGE
-    role grant [FLAGS] <role> <group>
+  @examples """
+  Granting the admin role to the ops group:
 
-  FLAGS
-    -h, --help  Display this usage info
-
-  ARGS
-    role   The name of the role to grant.
-    group  The name of the group to grant the role to
-
-  EXAMPLE
-
-    role grant admin-role dev-group
-
+    role grant admin ops
   """
 
-  def grant(%{options: %{"help" => true}}, _args),
-    do: show_usage
-  def grant(_req, [role, group]) when is_binary(role) and is_binary(group) do
-    case Roles.by_name(role) do
+  @output_description "Returns the serialized role with the group that was created"
+
+  @output_example """
+  [
+    {
+      "role": {
+        "permissions": [],
+        "name": "admin",
+        "id": "b711d91a-dcbe-4adc-862f-ea9144438955"
+      },
+      "group": {
+        "name": "ops",
+        "members": {
+          "users": [],
+          "roles": [],
+          "groups": []
+        },
+        "id": "2c9c8f77-6521-4b0c-b5cb-25548f83dcf2"
+      }
+    }
+  ]
+  """
+
+  permission "manage_roles"
+  permission "manage_groups"
+
+  rule "when command is #{Cog.Util.Misc.embedded_bundle}:role-grant must have #{Cog.Util.Misc.embedded_bundle}:manage_roles"
+  rule "when command is #{Cog.Util.Misc.embedded_bundle}:role-grant must have #{Cog.Util.Misc.embedded_bundle}:manage_roles"
+
+  def handle_message(req = %{args: [role, group]}, state) when is_binary(role) and is_binary(group) do
+    result = case Roles.by_name(role) do
       %Role{}=role ->
         case Groups.by_name(group) do
           {:ok, group} ->
@@ -42,14 +61,18 @@ defmodule Cog.Commands.Role.Grant do
       nil ->
         {:error, {:resource_not_found, "role", role}}
     end
-  end
-  def grant(_req, [_, _]),
-    do: {:error, :wrong_type}
-  def grant(_req, []),
-    do: {:error, {:not_enough_args, 2}}
-  def grant(_req, [_]),
-    do: {:error, {:not_enough_args, 2}}
-  def grant(_req, _),
-    do: {:error, {:too_many_args, 2}}
 
+    case result do
+      {:ok, template, data} ->
+        {:reply, req.reply_to, template, data, state}
+      {:error, err} ->
+        {:error, req.reply_to, Commands.Role.error(err), state}
+    end
+  end
+  def handle_message(req = %{args: [_, _]}, state),
+    do: {:error, req.reply_to, Commands.Role.error(:wrong_type), state}
+  def handle_message(req = %{args: args}, state) when length(args) < 2,
+    do: {:error, req.reply_to, Commands.Role.error({:not_enough_args, 2}), state}
+  def handle_message(req, state),
+    do: {:error, req.reply_to, Commands.Role.error({:too_many_args, 2}), state}
 end
