cbd4c21915e68265a764a99e342d7f32f3fbc80a
vanstee <patrick@vanstee.me>
2016-12-14 12:21:56 -0500

    Double the number of max mqtt clients

--- priv/config/ssl_mqtt.exs
+++ priv/config/ssl_mqtt.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :emqttd, :listeners,
   [{mqtt_type, mqtt_port,
     [acceptors: 8,
-     max_clients: 64,
+     max_clients: 128,
      access: [allow: :all],
      ssl: [certfile: cert,
            keyfile: key],
