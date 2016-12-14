0e0bd19f2eeccf11a1c4a5548f2be408a53ea5f8
Matthew Peck <matt@operable.io>
2016-12-14 13:46:52 -0500

    remove base rule command

--- lib/cog/commands/rule.ex
+++ lib/cog/commands/rule.ex
@@ -1,60 +1,6 @@
 defmodule Cog.Commands.Rule do
-  use Cog.Command.GenCommand.Base,
-    bundle: Cog.Util.Misc.embedded_bundle
 
-  #alias Cog.Commands.Rule.{List}
   require Cog.Commands.Helpers, as: Helpers
-
-  Helpers.usage(:root)
-
-  @description "Manage authorization rules"
-
-  @arguments "<subcommand>"
-
-  @subcommands %{
-    "list" => "List all rules (default)",
-    "info <id>" => "Retrieve a rule by ID",
-    "create [<rule> | <command> <permission>]" => "Add a rule",
-    "delete <id>" => "Delete a rule",
-  }
-
-  option "command", type: "string", short: "c"
-
-  permission "manage_commands"
-
-  rule "when command is #{Cog.Util.Misc.embedded_bundle}:rule must have #{Cog.Util.Misc.embedded_bundle}:manage_commands"
-
-  def handle_message(req, state) do
-    {subcommand, _args} = Helpers.get_subcommand(req.args)
-
-    result = case subcommand do
-      #"info" ->
-        #Info.info(req, args)
-      #"list" ->
-        #List.list(req, args)
-      #"create" ->
-        #Create.create(req, args)
-      #"delete" ->
-        #Delete.delete(req, args)
-      nil ->
-        if Helpers.flag?(req.options, "help") do
-          show_usage
-        else
-          #List.list(req, args)
-        end
-      other ->
-        {:error, {:unknown_subcommand, other}}
-    end
-
-    case result do
-      {:ok, template, data} ->
-        {:reply, req.reply_to, template, data, state}
-      {:ok, message} ->
-        {:reply, req.reply_to, message, state}
-      {:error, err} ->
-        {:error, req.reply_to, error(err), state}
-    end
-  end
 
   def error({:disabled, command}),
     do: "#{command} is not enabled. Enable a bundle version and try again"
