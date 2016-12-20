a79b107ad38d9e8492a123387d0ed6ead6a5c0bb
Christopher Maier <chris@operable.io>
2016-12-20 10:41:20 -0500

    Relax validation constraints on chat users
    Chat users don't need first and last names in order to successfully
    execute a pipeline.
    
    Fixes #1245

--- lib/cog/chat/user.ex
+++ lib/cog/chat/user.ex
@@ -5,8 +5,8 @@ defmodule Cog.Chat.User do
   field :id, :string, required: true
   field :provider, :string, required: true
   field :email, :string, required: false
-  field :first_name, :string, required: true
-  field :last_name, :string, required: true
+  field :first_name, :string, required: false
+  field :last_name, :string, required: false
   field :handle, :string, required: true
   field :mention_name, :string, required: false
 
