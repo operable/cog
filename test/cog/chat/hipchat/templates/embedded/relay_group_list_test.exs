28d7d6f564d4ba93a2b16576034b7c1e546e28e3
vanstee <patrick@vanstee.me>
2016-12-19 22:54:24 -0500

    Move tests to files named to pass all_templates_test

--- test/cog/chat/hipchat/templates/embedded/relay_group_list_test.exs
+++ test/cog/chat/hipchat/templates/embedded/relay_group_list_test.exs
@@ -15,39 +15,4 @@ defmodule Cog.Chat.HipChat.Templates.Embedded.RelayGroupListTest do
     assert_rendered_template(:hipchat, :embedded, "relay-group-list", data, expected)
   end
 
-  test "relay-group-list verbose template" do
-    data = %{"results" => [%{"name" => "foo",
-                             "relays" => [%{"name" => "relay1"},
-                                          %{"name" => "relay2"}],
-                             "bundles" => [%{"name" => "bundle1"},
-                                           %{"name" => "bundle2"}]},
-                           %{"name" => "bar",
-                             "relays" => [%{"name" => "relay1"},
-                                          %{"name" => "relay2"}],
-                             "bundles" => [%{"name" => "bundle1"},
-                                           %{"name" => "bundle2"}]},
-                           %{"name" => "baz",
-                             "relays" => [%{"name" => "relay1"},
-                                          %{"name" => "relay2"}],
-                             "bundles" => [%{"name" => "bundle1"},
-                                           %{"name" => "bundle2"}]}]}
-
-    expected =
-    """
-    <strong>Name:</strong> foo<br/>\
-    <strong>Relays:</strong> relay1, relay2<br/>\
-    <strong>Bundles:</strong> bundle1, bundle2<br/>\
-    <br/>\
-    <strong>Name:</strong> bar<br/>\
-    <strong>Relays:</strong> relay1, relay2<br/>\
-    <strong>Bundles:</strong> bundle1, bundle2<br/>\
-    <br/>\
-    <strong>Name:</strong> baz<br/>\
-    <strong>Relays:</strong> relay1, relay2<br/>\
-    <strong>Bundles:</strong> bundle1, bundle2
-    """ |> String.strip()
-
-    assert_rendered_template(:hipchat, :embedded, "relay-group-list-verbose", data, expected)
-  end
-
 end
