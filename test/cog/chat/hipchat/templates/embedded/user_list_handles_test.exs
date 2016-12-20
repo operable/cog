ff0acf4881cf32e95fc317cd8d47fb6f2134cefa
vanstee <patrick@vanstee.me>
2016-12-19 22:30:53 -0500

    Fix user template tests

--- test/cog/chat/hipchat/templates/embedded/user_list_handles_test.exs
+++ test/cog/chat/hipchat/templates/embedded/user_list_handles_test.exs
@@ -3,21 +3,28 @@ defmodule Cog.Chat.HipChat.Templates.Embedded.UserListHandlesTest do
 
   test "user-list-handles template" do
     data = %{"results" => [%{"username" => "cog",
-                             "handle" => "cog"},
+                             "handle" => "cog",
+                             "chat_provider" => %{"name" => "slack"}},
                            %{"username" => "sprocket",
-                             "handle" => "spacely"},
+                             "handle" => "spacely",
+                             "chat_provider" => %{"name" => "slack"}},
                            %{"username" => "chetops",
-                             "handle" => "ChetOps"}]}
+                             "handle" => "ChetOps",
+                             "chat_provider" => %{"name" => "hipchat"}}]}
+
     expected = """
-    <pre>+----------+----------+
-    | Username | Handle   |
-    +----------+----------+
-    | cog      | @cog     |
-    | sprocket | @spacely |
-    | chetops  | @ChetOps |
-    +----------+----------+
-    </pre>\
-    """
+    <strong>Username:</strong> cog<br/>
+    <strong>Handle:</strong> @cog<br/>
+    <strong>Chat Provider:</strong> slack<br/>
+    <br/>
+    <strong>Username:</strong> sprocket<br/>
+    <strong>Handle:</strong> @spacely<br/>
+    <strong>Chat Provider:</strong> slack<br/>
+    <br/>
+    <strong>Username:</strong> chetops<br/>
+    <strong>Handle:</strong> @ChetOps<br/>
+    <strong>Chat Provider:</strong> hipchat
+    """ |> String.replace("\n", "")
 
     assert_rendered_template(:hipchat, :embedded, "user-list-handles", data, expected)
   end
