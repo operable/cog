f44df3e6e2f535f79932812b871694ba64d9aab3
vanstee <patrick@vanstee.me>
2016-12-19 22:08:30 -0500

    Fix rule template tests

--- test/cog/chat/hipchat/templates/embedded/rule_list_test.exs
+++ test/cog/chat/hipchat/templates/embedded/rule_list_test.exs
@@ -12,15 +12,18 @@ defmodule Cog.Chat.HipChat.Templates.Embedded.RuleListTest do
                              "rule" => "when command is foo:baz allow",
                              "id" => "789"}]}
     expected = """
-    <pre>+---------+-------------------------------+-----+
-    | Command | Rule                          | ID  |
-    +---------+-------------------------------+-----+
-    | foo:foo | when command is foo:foo allow | 123 |
-    | foo:bar | when command is foo:bar allow | 456 |
-    | foo:baz | when command is foo:baz allow | 789 |
-    +---------+-------------------------------+-----+
-    </pre>\
-    """
+    <strong>Command:</strong> foo:foo<br/>
+    <strong>ID:</strong> 123<br/>
+    <strong>Rule:</strong><pre>when command is foo:foo allow</pre><br/>
+    <br/>
+    <strong>Command:</strong> foo:bar<br/>
+    <strong>ID:</strong> 456<br/>
+    <strong>Rule:</strong><pre>when command is foo:bar allow</pre><br/>
+    <br/>
+    <strong>Command:</strong> foo:baz<br/>
+    <strong>ID:</strong> 789<br/>
+    <strong>Rule:</strong><pre>when command is foo:baz allow</pre>
+    """ |> String.replace("\n", "")
 
     assert_rendered_template(:hipchat, :embedded, "rule-list", data, expected)
   end
