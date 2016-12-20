163d1c9e00ef24543aa8f4218cc47ff011b10365
vanstee <patrick@vanstee.me>
2016-12-19 22:29:14 -0500

    Fix trigger template tests

--- test/cog/chat/hipchat/templates/embedded/trigger_list_test.exs
+++ test/cog/chat/hipchat/templates/embedded/trigger_list_test.exs
@@ -4,7 +4,6 @@ defmodule Cog.Chat.HipChat.Templates.Embedded.TriggerListTest do
   test "trigger-list template" do
     data = %{"results" => [%{"id" => "abc123",
                              "name" => "test_trigger",
-                             "description" => "Tests things!",
                              "enabled" => true,
                              "pipeline" => "echo 'Something just happened'",
                              "as_user" => "bobby_tables",
@@ -12,52 +11,25 @@ defmodule Cog.Chat.HipChat.Templates.Embedded.TriggerListTest do
                              "invocation_url" => "https://cog.mycompany.com/invoke_stuff"},
                           %{"id" => "abc456",
                             "name" => "test_trigger_2",
-                            "description" => "Tests more things!",
                             "enabled" => false,
                             "pipeline" => "echo 'Something else just happened'",
                             "as_user" => "bobby_tables",
                             "timeout_sec" => 30,
                             "invocation_url" => "https://cog.mycompany.com/invoke_other_stuff"}]}
     expected = """
-    <pre>+--------+----------------+--------------------+----------+-------------------------------------+--------------+---------+----------------------------------------------+
-    | ID     | Name           | Description        | Enabled? | Pipeline                            | As User      | Timeout | Invocation URL                               |
-    +--------+----------------+--------------------+----------+-------------------------------------+--------------+---------+----------------------------------------------+
-    | abc123 | test_trigger   | Tests things!      | true     | echo 'Something just happened'      | bobby_tables | 30      | https://cog.mycompany.com/invoke_stuff       |
-    | abc456 | test_trigger_2 | Tests more things! | false    | echo 'Something else just happened' | bobby_tables | 30      | https://cog.mycompany.com/invoke_other_stuff |
-    +--------+----------------+--------------------+----------+-------------------------------------+--------------+---------+----------------------------------------------+
-    </pre>\
-    """
+    <strong>Name:</strong> test_trigger<br/>
+    <strong>ID:</strong> abc123<br/>
+    <strong>Enabled:</strong> true<br/>
+    <strong>Invocation URL:</strong> https://cog.mycompany.com/invoke_stuff<br/>
+    <strong>Pipeline:</strong> <code>echo 'Something just happened'</code><br/>
+    <br/>
+    <strong>Name:</strong> test_trigger_2<br/>
+    <strong>ID:</strong> abc456<br/>
+    <strong>Enabled:</strong> false<br/>
+    <strong>Invocation URL:</strong> https://cog.mycompany.com/invoke_other_stuff<br/>
+    <strong>Pipeline:</strong> <code>echo 'Something else just happened'</code>
+    """ |> String.replace("\n", "")
 
     assert_rendered_template(:hipchat, :embedded, "trigger-list", data, expected)
   end
-
-  test "handles null description, user" do
-    data = %{"results" => [%{"id" => "abc123",
-                             "name" => "test_trigger",
-                             "description" => "Tests things!",
-                             "enabled" => true,
-                             "pipeline" => "echo 'Something just happened'",
-                             "timeout_sec" => 30,
-                             "invocation_url" => "https://cog.mycompany.com/invoke_stuff"},
-                          %{"id" => "abc456",
-                            "name" => "test_trigger_2",
-                            "enabled" => false,
-                            "pipeline" => "echo 'Something else just happened'",
-                            "as_user" => "bobby_tables",
-                            "timeout_sec" => 30,
-                            "invocation_url" => "https://cog.mycompany.com/invoke_other_stuff"}]}
-    expected = """
-    <pre>+--------+----------------+---------------+----------+-------------------------------------+--------------+---------+----------------------------------------------+
-    | ID     | Name           | Description   | Enabled? | Pipeline                            | As User      | Timeout | Invocation URL                               |
-    +--------+----------------+---------------+----------+-------------------------------------+--------------+---------+----------------------------------------------+
-    | abc123 | test_trigger   | Tests things! | true     | echo 'Something just happened'      |              | 30      | https://cog.mycompany.com/invoke_stuff       |
-    | abc456 | test_trigger_2 |               | false    | echo 'Something else just happened' | bobby_tables | 30      | https://cog.mycompany.com/invoke_other_stuff |
-    +--------+----------------+---------------+----------+-------------------------------------+--------------+---------+----------------------------------------------+
-    </pre>\
-    """
-
-    assert_rendered_template(:hipchat, :embedded, "trigger-list", data, expected)
-
-  end
-
 end
