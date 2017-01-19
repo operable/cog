92677b6ebcb9c817f2f6c38cacc2a6372bb6bf18
Matthew Peck <matt@operable.io>
2017-01-19 15:33:50 -0500

    utilize repository pattern for roles

--- web/controllers/v1/role_controller.ex
+++ web/controllers/v1/role_controller.ex
@@ -2,32 +2,28 @@ defmodule Cog.V1.RoleController do
   use Cog.Web, :controller
 
   alias Cog.Models.Role
+  alias Cog.Repository.Roles
 
   plug Cog.Plug.Authentication
   plug Cog.Plug.Authorization, permission: "#{Cog.Util.Misc.embedded_bundle}:manage_roles"
 
   # Search by name only for now
   def index(conn, %{"name" => name}) do
-    role = Repo.get_by!(Role, name: name)
-    |> Repo.preload([[permissions: :bundle], [group_grants: :group]])
+    role = Roles.by_name(name)
     render(conn, "show.json", role: role)
   end
   def index(conn, _params) do
-    roles = Repo.all(Role)
-    |> Repo.preload([[permissions: :bundle], [group_grants: :group]])
+    roles = Roles.all()
     render(conn, "index.json", roles: roles)
   end
 
   def create(conn, %{"role" => params}) do
-    changeset = Role.changeset(%Role{}, params)
-
-    case Repo.insert(changeset) do
+    case Roles.new(params) do
       {:ok, role} ->
-        new_role = Repo.preload(role, [[permissions: :bundle], [group_grants: :group]])
         conn
         |> put_status(:created)
-        |> put_resp_header("location", role_path(conn, :show, new_role))
-        |> render("show.json", role: new_role)
+        |> put_resp_header("location", role_path(conn, :show, role))
+        |> render("show.json", role: role)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
@@ -36,17 +32,12 @@ defmodule Cog.V1.RoleController do
   end
 
   def show(conn, %{"id" => id}) do
-    role = Repo.get!(Role, id)
-    |> Repo.preload([[permissions: :bundle], [group_grants: :group]])
-     render(conn, "show.json", role: role)
+    role = Roles.by_id!(id)
+    render(conn, "show.json", role: role)
   end
 
-  def update(conn, %{"id" => id, "role" => role_params}) do
-    case Role
-    |> Repo.get!(id)
-    |> Repo.preload([[permissions: :bundle], [group_grants: :group]])
-    |> Role.changeset(role_params)
-    |> Repo.update do
+  def update(conn, %{"id" => id, "role" => params}) do
+    case Roles.by_id!(id) |> Roles.update(params) do
       {:ok, %Role{}=role} ->
         render(conn, "show.json", role: role)
       {:error, changeset} ->
@@ -57,9 +48,7 @@ defmodule Cog.V1.RoleController do
   end
 
   def delete(conn, %{"id" => id}) do
-    changeset = Role |> Repo.get!(id) |> Cog.Models.Role.changeset(:delete)
-
-    case Repo.delete(changeset) do
+    case Roles.by_id!(id) |> Roles.delete() do
       {:ok, _} ->
         send_resp(conn, :no_content, "")
       {:error, changed} ->
