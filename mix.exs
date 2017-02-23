9b7781adc63eeb546d4e8579299fd6355b61f366
Operable Engineering <engineering@operable.io>
2017-02-23 11:17:24 -0500

    Locking final dependencies for 1.0.0 release

--- mix.exs
+++ mix.exs
@@ -61,10 +61,10 @@ defmodule Cog.Mixfile do
     [
       # Operable Code
       ########################################################################
-      {:conduit, github: "operable/conduit", branch: "v1.0.0"},
-      {:probe, github: "operable/probe", branch: "v1.0.0"},
-      {:spanner, github: "operable/spanner", branch: "v1.0.0"},
-      {:greenbar, github: "operable/greenbar", branch: "v1.0.0"},
+      {:conduit, github: "operable/conduit", tag: "1.0.0"},
+      {:probe, github: "operable/probe", tag: "1.0.0"},
+      {:spanner, github: "operable/spanner", tag: "1.0.0"},
+      {:greenbar, github: "operable/greenbar", tag: "1.0.0"},
 
       # Used to model pipelines
       {:gen_stage, "~> 0.10.0"},
